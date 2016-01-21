5e5d281ab12a30dc6cc130c101dcf19f9a6c725f
Jxck <block.rxckin.beats@gmail.com>
2016-01-21 21:10:52 +0900

    up

--- blog/mdbuild.js
+++ blog/mdbuild.js
@@ -7,17 +7,9 @@ var parse = require("markdown-to-ast").parse,
 var traverse = require("txt-ast-traverse").traverse;
 
 var AST = parse(`
-# aaa
+# h1
 
-hoge
-
-## bbb
-
-fuga
-
-# ccc
-
-piyo
+aaa **bbb** ccc
 `)
 // var AST = parse(require('fs').readFileSync('./blog.md').toString())
 
@@ -80,189 +72,65 @@ function sectioning(children, depth) {
 AST.children = sectioning(AST.children, 1)
 
 
-function isInline(type) {
+function isInline(node) {
   return [
     Syntax.Str,
     Syntax.Header,
-  ].indexOf(type) > -1;
+    Syntax.Strong,
+    Syntax.Paragraph
+  ].indexOf(node.type) > -1;
+}
+
+
+
+var html = {
+  Document: (node)  => {
+    let value = ('\n' + node.value).replace(/\n/gm, '\n--');
+    return `<article>${value}\n</article>`
+  },
+  Section: (node)   => {
+    let value = ('\n' + node.value).replace(/\n/gm, '\n--');
+    return `<section>${value}\n</section>`
+  },
+  Paragraph: (node) => `<p>${node.value}</p>`,
+  Str: (node)       => node.value,
+  Header: (node)    => `<h${node.depth}>${node.value}</h${node.depth}>`,
+  Strong: (node)    => `<strong>${node.value}</strong>`,
 }
 
 var stack = [];
 traverse(AST, {
   enter(node) {
-    p("enter", node.type);
     node.inline = isInline(node.type)
     stack.unshift(node)
   },
   leave(node) {
-    p(stack);
-    debugger;
-    p("leave", node.type);
     if (node.value) { // value があったら
       // pop して
       let top = stack.shift();
       if (top.type !== node.type) console.error(top, node);
 
       // 閉じる
-      if (node.inline) {
-        stack.unshift({ tag: 'full', val: `<${node.type}>${node.value}</${node.type}>` })
-      } else {
-        stack.unshift({ tag: 'full', val: `<${node.type}>\n--${node.value}\n</${node.type}>` })
-      }
+      stack.unshift({ tag: 'full', val: html[node.type](node), inline: isInline(node) });
     } else {
       let vals = [];
 
       // 完成している兄弟タグを集めてきて配列に並べる
-      // インデントも下げておく
       while(stack[0].tag === 'full') {
         vals.unshift(stack.shift().val);
       }
       // 連結する
-      vals = vals.join('\n').replace(/\n/gm, '\n--')
+      vals = vals.join('\n').trim()
+
       // それを親タグで閉じる
       let top = stack.shift();
       if (top.type !== node.type) console.error(top, node);
 
-      if (node.inline) {
-        stack.unshift({ tag: 'full', val: `<${node.type}>${vals}</${node.type}>` })
-      } else {
-        stack.unshift({ tag: 'full', val: `<${node.type}>\n--${vals}\n</${node.type}>` })
-      }
+      node.value = vals
+      stack.unshift({ tag: 'full', val: html[node.type](node), inline: isInline(node) });
     }
   }
 });
 
 p(stack[0].val)
 
-traverse('', {
-  enter(node) {
-    // console.log("enter", node.type);
-    switch (node.type) {
-      case Syntax.Document:
-        html += bo`<body>`;
-        break;
-      case Syntax.Paragraph:
-        console.log(state)
-        //if (state.length === 1) {
-        //  html += `\n<p>`;
-        //}
-        html += bo`<p>`;
-        break;
-      case Syntax.BlockQuote:
-        html += bo`<blockquote>`;
-        // state.unshift(Syntax.BlockQuote);
-        break;
-      case Syntax.ListItem:
-        // state.unshift(Syntax.ListItem);
-        html += bo`<li>`;
-        break;
-      case Syntax.List:
-        html += node.ordered ? bo`<ol>`: bo`<ul>`;
-        break;
-      case Syntax.Header:
-        console.log(state);
-        for (;isFinite(state[0]) && state[0] >= node.depth;) {
-          state.shift();
-          html += bc`</section>`;
-        }
-        html += bo`<section>`;
-        state.unshift(node.depth)
-        html += bo`<h${node.depth}>`;
-        break;
-      case Syntax.CodeBlock:
-        html += bo`<pre lang="${node.lang}">`
-        html += node.value;
-        break;
-      case Syntax.HtmlBlock:
-        break;
-      case Syntax.ReferenceDef:
-        break;
-      case Syntax.Str:
-        html += node.raw
-        break;
-      case Syntax.Break:
-        break;
-      case Syntax.Emphasis:
-        html += io`<em>`;
-        break;
-      case Syntax.Strong:
-        html += io`<strong>`
-        break;
-      case Syntax.Html:
-        html += io`${node.value}`;
-        break;
-      case Syntax.Link:
-        html += io`<a href="${node.href}">`;
-        break;
-      case Syntax.Img:
-        break;
-      case Syntax.Code:
-        html += io`<code>${node.value}`;
-        break;
-      default:
-    }
-    state.unshift(node.type)
-  },
-  leave(node) {
-    // console.log("leave", node.type);
-    switch (node.type) {
-      case Syntax.Document:
-        for (;isFinite(state[0]);) {
-          html += bc`</section>`;
-          state.shift();
-        }
-        html += bc`</body>`;
-        break;
-      case Syntax.Paragraph:
-        //if (state.length === 2) {
-        //  html += `</p>`;
-        //}
-        html += bc`</p>`;
-        break;
-      case Syntax.BlockQuote:
-        // state.shift();
-        html += bc`</blockquote>`;
-        break;
-      case Syntax.ListItem:
-        html += bc`</li>`;
-        // state.shift();
-        break;
-      case Syntax.List:
-        html += node.ordered ? bc`</ol>`: bc`</ul>`;
-        break;
-      case Syntax.Header:
-        html += bc`</h${node.depth}>`;
-        break;
-      case Syntax.CodeBlock:
-        html += bc`</pre>`;
-        break;
-      case Syntax.HtmlBlock:
-        break;
-      case Syntax.ReferenceDef:
-        break;
-      case Syntax.Str:
-        break;
-      case Syntax.Break:
-        break;
-      case Syntax.Emphasis:
-        html += ic`</em>`;
-        break;
-      case Syntax.Strong:
-        html += ic`</strong>`;
-        break;
-      case Syntax.Html:
-        break;
-      case Syntax.Link:
-        html += ic`</a>`;
-        break;
-      case Syntax.Image:
-        html += ic`<img src="${node.src}" alt="${node.alt}" title="${node.title}">`
-        break;
-      case Syntax.Code:
-        html += ic`</code>`;
-        break;
-      default:
-    }
-    state.shift()
-  }
-});
