0eb7fb016cd7e561d021770ce05f663352ed1031
Jxck <block.rxckin.beats@gmail.com>
2016-02-02 23:24:38 +0900

    line break after blockquote tag

--- blog.jxck.io/mark.js
+++ blog.jxck.io/mark.js
@@ -84,7 +84,7 @@ ${article}
   Paragraph:  (node) => `<p>${node.value}\n`,
   CodeBlock:  (node) => `<pre class=${node.lang}><code>${node.value}</code></pre>\n`,
   Code:       (node) => h`<code>${node.value}</code>`,
-  BlockQuote: (node) => h`<blockquote>${node.value}</blockquote>`,
+  BlockQuote: (node) => h`<blockquote>${node.value}</blockquote>\n`,
   ListItem:   (node) => `<li>${node.value}\n`,
   Link:       (node) => `<a href="${node.href}">${node.value}</a>`,
   Image:      (node) => `<img src=${node.src} alt="${node.alt}" title="${node.title}" >`,
