96cd3a411b38c7f5d5de38bada641a8ed927e874
Jxck <block.rxckin.beats@gmail.com>
2016-02-03 19:30:06 +0900

    add index build.rb

---
+++ blog.jxck.io/build.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+entries = `find ./entries -name *.md`.split("\n").map{|file|
+  `./mark.js #{file.sub("./", "")}`
+  head = `head -n 1 #{file}`
+
+  title = head.match(/\# \[.*\] (.*)/)[1]
+
+  tags = head
+    .scan(/\[(.+?)\]/)
+    .map{|s| s.pop }
+    .map{|tag| "<a>#{tag}</a>" }
+    .join
+
+  tagspan = "<span class=tags>#{tags}</span>"
+
+  x, y, date, name = file.split("/")
+  name = name.gsub(".md", ".html")
+  "<li><time datetime=#{date}>#{date}</time><a href=entries/#{date}/#{name}>#{title}</a>#{tagspan}"
+}
+
+puts <<-EOS
+<!DOCTYPE html>
+<meta charset=utf-8>
+<meta name=viewport content="width=device-width,initial-scale=1">
+
+<title>blog.jxck.io</title>
+<link rel=stylesheet type=text/css href=assets/style.css>
+
+<header>
+  <a class=logo href=/>blog.jxck.io</a>
+</header>
+
+<section class=archive>
+  <h1>Archive</h1>
+  <h2>2016</h2>
+  <ul>
+    #{entries.join("\n    ")}
+  </ul>
+</section>
+
+<hr>
+
+<footer>
+  <address class=copyright>Copyright &copy; 2016 <a href=/>Jxck</a>. All Rights Reserved.</address>
+</footer>
+EOS
