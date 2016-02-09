8eb35cedee69717bcc7fe0ad51468617fa0d6122
Jxck <block.rxckin.beats@gmail.com>
2016-02-09 21:59:55 +0900

    fixup index.rb

--- .mruby.handler/index.rb
+++ .mruby.handler/index.rb
@@ -1,25 +1,53 @@
-def list(path)
-  li = Dir.entries(path)
-     .reject{|path| path == "." }
+def list(dir, path)
+  li = Dir.entries("#{dir}/#{path}")
+     .reject{|file| file == "." }
      .sort()
-     .map{|path| "<li><a href=#{path}>#{path}</a></li>" }
+     .map{|file|
+       "<li><a href=/#{path}/#{file}>#{file}</a></li>"
+     }
      .join("\n")
+
   <<-EOS
-<html>
-<ul>
-#{li}
-</ul>
-</html>
+<!DOCTYPE html>
+<meta charset=utf-8>
+<meta name=viewport content="width=device-width,initial-scale=1">
+
+<title>blog.jxck.io</title>
+<link rel=stylesheet type=text/css href=//www.jxck.io/assets/css/style.css>
+
+<header>
+  <div>
+    <a class=logo href=//jxck.io><img alt="jxck logo" src=//jxck.io/assets/img/jxck.png></a>
+    <a class=path href=/>blog.jxck.io</a>
+    <a class=feed href=/feeds/atom.xml><img src=//jxck.io/assets/img/rss.svg></a>
+  </div>
+</header>
+
+<section class=archive>
+  <h1><a href=##{path}>#{path}</a></h1>
+  <ul>
+    #{li}
+  </ul>
+</section>
+
+<hr>
+
+<footer>
+  <address class=copyright>Copyright &copy; 2016 <a href=/>Jxck</a>. All Rights Reserved.</address>
+</footer>
 EOS
 end
 
 Proc.new do |env|
   path = env["PATH_INFO"]
+  dir = "./blog.jxck.io"
 
   if path == "/entries" || path == "/entries/"
-    [200, {"content-type" => "text/html"}, [list("./blog.jxck.io/entries")]]
+    [200, {"content-type" => "text/html"}, [list(dir, "entries")]]
   elsif path =~ /\/entries\/(\d{4}-\d{2}-\d{2})(\/{0,1})$/
-    [200, {"content-type" => "text/html"}, [list("./blog.jxck.io/entries/#{$1}")]]
+    [200, {"content-type" => "text/html"}, [list(dir, "entries/#{$1}")]]
+  elsif path =~ /.*.md/
+    [200, {"content-type" => "text/plain"}, [File.read(dir + path)]]
   else
     [399, {}, []]
   end
