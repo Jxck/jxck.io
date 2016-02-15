3472b0d1ebc26ab25f21c22f2fb9cc205c2446ce
Jxck <block.rxckin.beats@gmail.com>
2016-02-16 04:18:26 +0900

    [build] export template from build.rb

--- build.rb
+++ build.rb
@@ -27,38 +27,10 @@ entries = Dir.glob("./blog.jxck.io/entries/**/**")
     top = url if i == 0
     "<li><time datetime=#{date}>#{date}</time><a href=#{url}>#{title}</a>#{tagspan}"
   }
+  .join("\n    ")
 
-puts <<-EOS
-<!DOCTYPE html>
-<meta charset=utf-8>
-<meta name=viewport content="width=device-width,initial-scale=1">
+html = File.read(".template/archive.html")
+html = html.gsub('"', '\"')
+html = eval('"' + html + '"')
 
-<title>blog.jxck.io</title>
-<link rel=prerender href=#{top}>
-<link rel=stylesheet type=text/css href=//www.jxck.io/assets/css/body.css>
-
-<link rel=stylesheet type=text/css href=//www.jxck.io/assets/css/header.css>
-<header>
-  <div>
-    <a class=logo href=//jxck.io><img alt="jxck logo" src=//jxck.io/assets/img/jxck.png></a>
-    <a class=path href=/>blog.jxck.io</a>
-    <a class=feed href=/feeds/atom.xml><img alt="rss feed" src=//jxck.io/assets/img/rss.svg></a>
-  </div>
-</header>
-
-<link rel=stylesheet type=text/css href=//www.jxck.io/assets/css/main.css>
-<main class=archive>
-  <h1><a href=#archive>Archive</a></h1>
-  <h2><a href=#2016>2016</a></h2>
-  <ul>
-    #{entries.join("\n    ")}
-  </ul>
-</main>
-
-<hr>
-
-<link rel=stylesheet type=text/css href=//www.jxck.io/assets/css/footer.css>
-<footer>
-  <address class=copyright>Copyright &copy; 2016 <a href=/>Jxck</a>. All Rights Reserved.</address>
-</footer>
-EOS
+puts html
