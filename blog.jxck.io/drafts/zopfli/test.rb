344bdacf75d64d62561650a95c24ad4893a38555
Jxck <block.rxckin.beats@gmail.com>
2016-02-17 00:01:09 +0900

    [draft] add zopfli

---
+++ blog.jxck.io/drafts/zopfli/test.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+`rm *.gz`
+`rm result`
+`touch result`
+(1..10).map{|i| i * 10 }.each{|i|
+  `time -v -a -o result zopfli --i#{i} -c loading-css-over-http2.html > loading-css-over-http2.html.#{i}.gz`
+}
