3a884f66fdb2abe504d0c0dcda75509483f6ad78
Jxck <block.rxckin.beats@gmail.com>
2016-02-23 08:25:48 +0900

    [feed] update feed xml

--- feed.rb
+++ feed.rb
@@ -25,27 +25,27 @@ entries = Dir.glob("./blog.jxck.io/entries/**/**")
   updated = "#{date}T00:00:00Z"
 
   <<-EOS
-  <entry>
-   <title>#{title}</title>
-   <link href="#{href}"/>
-   <id>tag:blog.jxck.io,2016:entry://#{date}</id>
-   <updated>#{updated}</updated>
-   <summary>#{summary}</summary>
-  </entry>
+<entry>
+ <title>#{title}</title>
+ <link href="#{href}" rel="altenate" />
+ <id>tag:blog.jxck.io,2016:entry://#{date}</id>
+ <updated>#{updated}</updated>
+ <summary>#{summary}</summary>
+</entry>
   EOS
 }
 
 feed = <<-EOS
 <?xml version='1.0' encoding='UTF-8'?>
 <feed xmlns='http://www.w3.org/2005/Atom' xml:lang='ja'>
- <title>blog.jxck.io</title>
- <link href="https://blog.jxck.io/"/>
- <link rel="self" type="application/atom+xml" href="https://blog.jxck.io/feeds/atom.xml"/>
- <updated>2016-01-28T18:30:02Z</updated>
- <author>
-   <name>Jxck</name>
- </author>
- <id>tag:blog.jxck.io,2016:feed</id>
+<title>blog.jxck.io</title>
+<link rel="alternate" href="https://blog.jxck.io/"/>
+<link rel="self" type="application/atom+xml" href="https://blog.jxck.io/feeds/atom.xml"/>
+<author><name>Jxck</name></author>
+<icon>https://www.jxck.io/favicon.ico</icon>
+<logo>https://www.jxck.io/assets/img/jxck.png</logo>
+<id>tag:blog.jxck.io,2016:feed</id>
+<updated>2016-01-28T18:30:02Z</updated>
 #{entries.join("\n")}
 </feed>
 EOS
