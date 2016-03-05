105e431a79513bc725d8c3b638ad31672cc533a7
Jxck <block.rxckin.beats@gmail.com>
2016-03-05 16:36:53 +0900

    [labs/sw] add service worker lab

--- labs.jxck.io/service-worker/master.js
+++ labs.jxck.io/service-worker/master.js
@@ -1,25 +1,25 @@
 'use strict';
 
-// window
-if (navigator.serviceWorker === undefined) {
-  alert('service worker not supported');
-}
 
-navigator.serviceWorker.addEventListener('controllerchange', console.log.bind(console));
+navigator.serviceWorker.register('worker.js?ver=6').then((registration) => {
+  registration.addEventListener('updatefound', (e) => {
+    console.log('updatefound', e);
+    console.log('installing', registration.installing);
+    console.log('waiting', registration.waiting);
+    console.log('active', registration.active);
+  });
 
-navigator.serviceWorker.register('worker.js', { scope: '.' }).then((worker) => {
-  // console.log('register success', worker.scope);
+  console.log(navigator.serviceWorker.controller);
   return navigator.serviceWorker.ready;
-}).then((worker) => {
-  // console.log('active worker', worker);
-  console.log('controller', navigator.serviceWorker.controller);
+}).then((registration) => {
+  if (navigator.serviceWorker.controller) {
+    return registration;
+  }
+  return new Promise((resolve, reject) => {
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      resolve(registration);
+    });
+  });
+}).then((registration) => {
+  console.log(registration);
 }).catch(console.error.bind(console));
-
-
-//window.addEventListener('DOMContentLoaded', () => {
-//  setInterval(() => {
-//    fetch('/test').then((res) => {
-//      console.log(res);
-//    });
-//  }, 1000);
-//});
