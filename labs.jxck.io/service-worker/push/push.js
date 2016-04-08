2d65c8dada257967c31ccd54d8faabc47f9408ec
Jxck <block.rxckin.beats@gmail.com>
2016-04-08 18:36:08 +0900

    [labs/push] update push.js

--- labs.jxck.io/service-worker/push/push.js
+++ labs.jxck.io/service-worker/push/push.js
@@ -1,39 +1,50 @@
+#!/usr/bin/env node
+
 var https = require('https');
 
-var AuthKey = "key=AIzaSyBBh4ddPa96rQQNxqiq_qQj7sq1JdsNQUQ";
+var Key = "key=AIzaSyBBh4ddPa96rQQNxqiq_qQj7sq1JdsNQUQ";
+var IDs = ["c6Wbq7isMLU:APA91bFl9FiNmTiylACCZWc_DnpnbYvy-mcSqhN49kcE6AFk36Mm9nQXbf1vaFjYkZDdVRTIo0aIz4yZ2kQSbzxaQCFCKgjGwIccODcIZ37oCYbv074NsMmcF0UM5Xb973DCnbFkExJR"];
 
-var data = JSON.stringify({
-  "registration_ids": ["c6Wbq7isMLU:APA91bFl9FiNmTiylACCZWc_DnpnbYvy-mcSqhN49kcE6AFk36Mm9nQXbf1vaFjYkZDdVRTIo0aIz4yZ2kQSbzxaQCFCKgjGwIccODcIZ37oCYbv074NsMmcF0UM5Xb973DCnbFkExJR"],
-})
-
-var options = {
-  hostname: 'android.googleapis.com',
-  port: 443,
-  path: '/gcm/send',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': AuthKey,
-    'Content-Length': data.length
-  }
-};
-
-var req = https.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
+function Push(key, ids) {
+  'use strict';
+  let body = JSON.stringify({
+    registration_ids: ids
   });
-  res.on('end', () => {
-    console.log('No more data in response.')
-  })
-});
+  let options = {
+    hostname: 'android.googleapis.com',
+    port: 443,
+    path: '/gcm/send',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': key,
+      'Content-Length': body.length
+    },
+  }
+  return new Promise((resolve, reject) => {
+    let result = [];
+    let req = https.request(options, (res) => {
+      result.push(res.statusCode);
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        result.push(chunk);
+      });
+      res.on('end', () => {
+        resolve(result);
+      })
+    });
 
-req.on('error', (e) => {
-  console.log(`problem with request: ${e.message}`);
-});
+    req.on('error', (e) => {
+      reject(e);
+    });
 
-// write data to request body
-req.write(data);
-req.end();
+    req.write(body);
+    req.end();
+  });
+}
+
+Push(Key, IDs).then((res) => {
+  console.log(res.join(' '));
+}).catch((err) => {
+  console.error(err);
+});
