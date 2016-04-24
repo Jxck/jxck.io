3781a56866f28db8c02f8672cb86a3bde854ff91
Jxck <block.rxckin.beats@gmail.com>
2016-04-24 15:50:44 +0900

    [labs/sw] 1 init

--- labs.jxck.io/service-worker/registration/worker.js
+++ labs.jxck.io/service-worker/registration/worker.js
@@ -1,11 +1,10 @@
-const v = 2;
-console.info(` worker${v}`);
+console.info(` worker`);
 
-self.addEventListener('install', (e) => {
-  console.info(` install${v}`);
-  //e.waitUntil(skipWaiting());
-});
-
-self.addEventListener('activate', (e) => {
-  console.info(` activate${v}`);
+self.addEventListener('fetch', (e) => {
+  let path = new URL(e.request.url).pathname;
+  console.log(path);
+  if (path === '/test') {
+    e.respondWith(new Response('test'));
+  }
+  return;
 });
