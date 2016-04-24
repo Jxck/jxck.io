d0a66bc54b6b074a26c239f353fd00c1b3bca05c
Jxck <block.rxckin.beats@gmail.com>
2016-04-24 15:58:24 +0900

    [labs/sw] 2 claim

--- labs.jxck.io/service-worker/registration/main.js
+++ labs.jxck.io/service-worker/registration/main.js
@@ -3,3 +3,9 @@ console.log('master');
 navigator.serviceWorker.register('worker.js').then((registration) => {
   console.log(registration);
 });
+
+document.getElementById('test').addEventListener('click', () => {
+  fetch('test').then((e) => {
+    console.log(e);
+  });
+});
--- labs.jxck.io/service-worker/registration/worker.js
+++ labs.jxck.io/service-worker/registration/worker.js
@@ -1,5 +1,11 @@
 console.info(` worker`);
 
+
+self.addEventListener('activate', (e) => {
+  console.info(' activate', e);
+  e.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', (e) => {
   let path = new URL(e.request.url).pathname;
   console.log(path);
