55efbad70c21afcc5062aa5488c812559952672b
Jxck <block.rxckin.beats@gmail.com>
2016-04-29 12:13:55 +0900

    [labs/sw] update cache

--- labs.jxck.io/service-worker/cache/master.js
+++ labs.jxck.io/service-worker/cache/master.js
@@ -6,8 +6,10 @@ navigator.serviceWorker.register('worker.js').then((registration) => {
 });
 
 let n = 0;
-
 let timer = null;
+
+let log = document.getElementById('log');
+
 document.getElementById('cache').addEventListener('click', () => {
   if (timer !== null) {
     clearInterval(timer);
@@ -18,7 +20,8 @@ document.getElementById('cache').addEventListener('click', () => {
     let chan = new MessageChannel();
     let message = JSON.stringify({ n: n++ });
     navigator.serviceWorker.controller.postMessage(message, [chan.port2]);
-    navigator.storageQuota.queryInfo("temporary").then(console.log.bind(console));
+    navigator.storageQuota.queryInfo("temporary").then((info) => {
+      log.value += (`cache: ${n}, usage: ${info.usage}, quota: ${info.quota}\n`);
+    });
   }, 1000);
 });
-
--- labs.jxck.io/service-worker/cache/worker.js
+++ labs.jxck.io/service-worker/cache/worker.js
@@ -2,24 +2,15 @@ console.info('worker');
 
 self.addEventListener('install', (e) => {
   console.info('install', e);
-  e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          return caches.delete(cacheName);
-        })
-      );
-    }).catch(console.error.bind(console))
-  );
 });
 
 self.addEventListener('message', (message) => {
-  const n = JSON.parse(message.data).n;
   const m = 10;
+  const n = JSON.parse(message.data).n;
   const key = '/service-worker/random';
   return caches.open(key).then((cache) => {
     let urls = Array.from(Array(m).keys()).map((i) => `${key}?${i+m*n}`);
-    console.info(`cache ${n} requests`);
+    console.info(`cache ${m*n} requests`);
     return cache.addAll(urls);
   });
 });
