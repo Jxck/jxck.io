399a4fa3f0cdf170d724b77a3f4ba3f5150d2809
Jxck <block.rxckin.beats@gmail.com>
2016-05-02 01:17:21 +0900

    [labs/sw] fixup sync

--- labs.jxck.io/service-worker/backgroundsync/master.js
+++ labs.jxck.io/service-worker/backgroundsync/master.js
@@ -1,5 +1,9 @@
 navigator.serviceWorker.register('worker.js').then((registration) => {
   return navigator.serviceWorker.ready;
 }).then((registration) => {
-  return registration.sync.register('update-cache');
+  document.getElementById('button').addEventListener('click', () => {
+    registration.sync.register('update-cache').then(() => {
+      console.log('sync registered');
+    }).catch(console.error.bind(console));
+  });
 }).catch(console.error.bind(console));
