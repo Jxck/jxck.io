ba5a7b8f6c6e5a7aed0087925653b3f66b21e7cd
Jxck <block.rxckin.beats@gmail.com>
2016-05-03 17:20:37 +0900

    [build.rb] refuctoring

--- build.rb
+++ build.rb
@@ -6,33 +6,33 @@ puts "blog html deleted"
 
 top = ""
 entries = Dir.glob("./blog.jxck.io/entries/**/**")
-             .select { |path| path.match(/.*.md$/) }
-             .sort { |a, b| b <=> a }
-             .map.with_index { |file, i|
-               puts "process #{file}"
+  .select { |path| path.match(/.*.md$/) }
+  .sort { |a, b| b <=> a }
+  .map.with_index { |file, i|
+    puts "process #{file}"
 
-               `./mark.js #{file}`
-               head = `head -n 1 #{file}`
+    `./mark.js #{file}`
+    head = `head -n 1 #{file}`
 
-               title = head.match(/\# \[.*\] (.*)/)[1]
+    title = head.match(/\# \[.*\] (.*)/)[1]
 
-               tags = head
-                      .scan(/\[(.+?)\]/)
-                      .map(&:pop)
-                      .map { |tag| "<a>#{tag}</a>" }
-                      .reverse
-                      .join(",")
+    tags = head
+      .scan(/\[(.+?)\]/)
+      .map(&:pop)
+      .map { |tag| "<a>#{tag}</a>" }
+      .reverse
+      .join(",")
 
-               tagspan = "<span class=tags>[#{tags}]</span>"
+    tagspan = "<span class=tags>[#{tags}]</span>"
 
-               splitted = file.split("/")
-               name = splitted.pop.gsub(".md", ".html")
-               date = splitted.pop
-               url = "entries/#{date}/#{name}"
-               top = url if i == 0
-               "<li><time datetime=#{date}>#{date}</time><a href=#{url}>#{title}</a>#{tagspan}"
-             }
-             .join("\n    ")
+    splitted = file.split("/")
+    name = splitted.pop.gsub(".md", ".html")
+    date = splitted.pop
+    url = "entries/#{date}/#{name}"
+    top = url if i == 0
+    "<li><time datetime=#{date}>#{date}</time><a href=#{url}>#{title}</a>#{tagspan}"
+  }
+  .join("\n    ")
 
 html = File.read(".template/archive.html")
 html = html.gsub('"', '\"')
