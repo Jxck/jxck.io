4b6d98df7b04948013d19db9fd7b8d98f41fd9b8
Jxck <block.rxckin.beats@gmail.com>
2016-05-05 00:43:00 +0900

    [build.rb] refuctoring build.rb

--- build.rb
+++ build.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
+# .gz 削除
+`find ./blog.jxck.io/* -name *.gz -delete`
+puts "blog gz deleted"
+
 # html, amp.html 削除
 `rm -rf ./blog.jxck.io/entries/**/*.html`
 puts "blog html deleted"
@@ -14,10 +18,13 @@ entries = Dir.glob("./blog.jxck.io/entries/**/**")
   .select { |path| path.match(/.*.md$/) }
   .sort { |a, b| b <=> a }
   .map.with_index { |file, i|
-    head = `head -n 1 #{file}`
+    # read first line
+    head = open(file) {|f| f.gets }
 
+    # get title
     title = head.match(/\# \[.*\] (.*)/)[1]
 
+    # get tags
     tags = head
       .scan(/\[(.+?)\]/)
       .map(&:pop)
