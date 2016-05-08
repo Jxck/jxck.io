a9af541a3e8335b203661f2643a3b33a81651d8f
Jxck <block.rxckin.beats@gmail.com>
2016-05-08 21:00:39 +0900

    [feed] refuctoring feed.rb

--- feed.rb
+++ feed.rb
@@ -11,11 +11,14 @@ def hsp(str)
      .gsub(/'/, "&#039;")
 end
 
-entries = Dir.glob("./blog.jxck.io/entries/**/**")
+def Feed(dir)
+  host = dir.split("/")[1]
+
+  entries = Dir.glob(dir)
   .select { |path| path.match(/.*.md\z/) }
   .sort { |a, b| b <=> a }
   .map { |name|
-    href     = name.gsub("./blog.jxck.io/", "https://blog.jxck.io/").gsub(".md", ".html")
+    href     = name.gsub("./#{host}/", "https://#{host}/").gsub(".md", ".html")
     file     = File.open(name)
     text     = file.read
     title    = text.match(/^# \[.*\] (.*)/)[1]
@@ -23,7 +26,7 @@ entries = Dir.glob("./blog.jxck.io/entries/**/**")
     summary  = hsp(summary)
     splitted = name.split("/")
     date     = splitted[3]
-    id       = "tag:blog.jxck.io,2016:entry://#{date}"
+    id       = "tag:#{host},2016:entry://#{date}"
     updated  = "#{date}T00:00:00Z"
 
     {
@@ -35,8 +38,8 @@ entries = Dir.glob("./blog.jxck.io/entries/**/**")
     }
   }
 
-xml_entries = entries.map{|e|
-  <<-EOS
+  xml_entries = entries.map{|e|
+    <<-EOS
   <entry>
    <title>#{e[:title]}</title>
    <link href="#{e[:href]}" rel="alternate" />
@@ -44,29 +47,33 @@ xml_entries = entries.map{|e|
    <updated>#{e[:updated]}</updated>
    <summary>#{e[:summary]}</summary>
   </entry>
-  EOS
-}.join("\n")
+    EOS
+  }.join("\n")
 
-xml = <<-EOS
+  xml = <<-EOS
 <?xml version='1.0' encoding='UTF-8'?>
 <feed xmlns='http://www.w3.org/2005/Atom' xml:lang='ja'>
-<title>blog.jxck.io</title>
-<link rel="alternate" href="https://blog.jxck.io/"/>
-<link rel="self" type="application/atom+xml" href="https://blog.jxck.io/feeds/atom.xml"/>
+<title>#{host}</title>
+<link rel="alternate" href="https://#{host}/"/>
+<link rel="self" type="application/atom+xml" href="https://#{host}/feeds/atom.xml"/>
 <author><name>Jxck</name></author>
-<id>tag:blog.jxck.io,2016:feed</id>
+<id>tag:#{host},2016:feed</id>
 <updated>2016-01-28T18:30:02Z</updated>
 #{xml_entries}
 </feed>
-EOS
-File.write("blog.jxck.io/feeds/atom.xml", xml)
+  EOS
+  File.write("#{host}/feeds/atom.xml", xml)
 
-json = JSON.pretty_generate(
-  title:     "blog.jxck.io",
-  alternate: "https://blog.jxck.io",
-  author:    { name: "jxck" },
-  id:        "tag:blog.jxck.io,2016:feed",
-  update:    "2016-01-28T18:30:02Z",
-  entry:     entries
-)
-File.write("blog.jxck.io/feeds/atom.json", json)
+  json = JSON.pretty_generate(
+    title:     host,
+    alternate: "https://#{host}",
+    author:    { name: "Jxck" },
+    id:        "tag:#{host},2016:feed",
+    update:    "2016-01-28T18:30:02Z",
+    entry:     entries
+  )
+
+  File.write("#{host}/feeds/atom.json", json)
+end
+
+Feed("./blog.jxck.io/entries/**/**")
