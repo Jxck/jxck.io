0d78b9c18c2ea505eb17b00a4f369cbe66c0608c
Jxck <block.rxckin.beats@gmail.com>
2016-06-08 22:01:12 +0900

    [tmp] add sw.push.js

--- www.jxck.io/assets/js/sw.push.js
+++ www.jxck.io/assets/js/sw.push.js
@@ -1,5 +1,75 @@
 'use strict';
 
+navigator.serviceWorker.register('worker.js').then((registration) => {
+  return navigator.serviceWorker.ready;
+}).then((registration) => {
+  return registration.pushManager.subscribe({ userVisibleOnly: true, });
+})
+
+// TODO
+((registration) => {
+
+  Notification.requestPermission((permission) => {
+    if (permission !== 'denied') {
+      sw.pushManager.subscribe().then((subscription) => {
+        //
+      });
+    }
+  });
+
+  registration.pushManager.hasPermission().then((pushPermissionStatus) => {
+    // Once we have a service worker, and checked permission,
+    // enable the buttons
+    var buttonContainer = document.querySelector('.button-container');
+    buttonContainer.style.display = 'block';
+
+    // If we don't have permission then set the UI accordingly
+    if (pushPermissionStatus !== 'granted') {
+      changeState(STATE_NOTIFICATION_PERMISSION);
+      return;
+    }
+
+    // We have permission, so let's update the subscription
+    // just to be safe
+    serviceWorkerRegistration.pushManager.getSubscription().then((pushSubscription) => {
+      // Check if we have an existing pushSubscription
+      if (pushSubscription) {
+        sendSubscription(pushSubscription);
+        changeState(STATE_ALLOW_PUSH_SEND);
+      } else {
+        changeState(STATE_NOTIFICATION_PERMISSION);
+      }
+    });
+  });
+
+
+
+  const endpoint = subscription.endpoint;
+  const auth = subscription.getKey('auth');
+  const p256dh = subscription.getKey('p256dh');
+
+  const userAuth = btoa(String.fromCharCode(...new Uint8Array(auth)));
+  const userPublicKey = btoa(String.fromCharCode(...new Uint8Array(p256dh)));
+
+  const body = JSON.stringify({ endpoint, userAuth, userPublicKey });
+  console.log(JSON.stringify(subscription, ' ', ' '));
+  console.log(body);
+
+  const url = 'wss://ws.jxck.io';
+  const protocol = 'push_register';
+  let ws = new WebSocket(url, protocol);
+
+  ws.addEventListener('message', (e) => {
+    console.log('message', e);
+  });
+  ws.addEventListener('open', () => {
+    ws.send(body);
+  });
+  ws.addEventListener('error', (e) => {
+    console.error(e);
+  });
+})();
+
 // generic push notification handler
 // show notification -> open tab when clicked
 // pushed data should have
