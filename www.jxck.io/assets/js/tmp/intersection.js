76cb29a6351d44564bc48ac7fe06f1cad7ef5e69
Jxck <block.rxckin.beats@gmail.com>
2016-06-20 01:21:51 +0900

    [js] move unused js to tmp

---
+++ www.jxck.io/assets/js/tmp/intersection.js
@@ -0,0 +1,57 @@
+'use strict';
+let p = console.log.bind(console);
+
+let hr = document.createElement('hr');
+
+function load(path) {
+  fetch(path)
+    .then((res) => res.text())
+    .then((text) => {
+      // fetch & parse prev html
+      let parser = new DOMParser();
+      let doc = parser.parseFromString(text, 'text/html');
+
+      // update link prev/next
+      let prev = doc.querySelector('link[rel=prev]').href;
+      let next = doc.querySelector('link[rel=next]').href;
+      let title = doc.querySelector('title').textContent;
+      document.querySelector('link[rel=prev]').href = prev;
+      document.querySelector('link[rel=next]').href = next;
+
+      // append prev article to main
+      let article = doc.querySelector('article');
+      let main = document.querySelector('main');
+      main.appendChild(hr);
+      main.appendChild(article);
+
+      // update current location & title
+      history.pushState(title, title, path);
+      document.querySelector('title').textContent = title;
+
+      window.addEventListener('popstate',function(e){
+        console.log(e.state);
+      });
+
+    });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  let observer = new IntersectionObserver((changes) => {
+    for (let change of changes) {
+      if (change.intersectionRatio > 0 && change.target.nodeName === 'FOOTER') {
+        let next = document.querySelector('link[rel=prev]').href;
+        let path = new URL(next).pathname;
+        console.log('fetch', path);
+        load(path);
+      }
+    }
+  });
+  observer.observe(document.querySelector('footer'));
+
+
+
+
+
+
+  //console.log(hr);
+});
