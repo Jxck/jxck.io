b7a528eb28948e86497f24e3f5d75be1369b2b9b
Jxck <block.rxckin.beats@gmail.com>
2016-08-28 14:31:29 +0900

    [font] fixup comment

--- www.jxck.io/assets/font/font.rb
+++ www.jxck.io/assets/font/font.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
 # このスクリプトを実行すると、現時点のエントリの中から All.txt に入ってない文字だけを出す。
-# この出力を All.txt に入れて、フォントを生成し直し、バージョンをつけてデプロイする。
+# この出力を All.txt に入れてフォントセットを作り直す。
+# そこから https://noto-website.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip をベースに
+# http://opentype.jp/subsetfontmk.htm を使って生成し
+# http://opentype.jp/woffconv.htm で woff にする
+# バージョンをつけてデプロイする。
 current = Dir.glob("../../../blog.jxck.io/entries/**/*.md").select{|file|
   file != "../../../blog.jxck.io/entries/2016-03-14/web-font-noto-sans.md"
 }.map{|file|
