ba12c1751108c9faa89fe9de95e0d63900919eef
Jxck <block.rxckin.beats@gmail.com>
2016-08-28 14:23:15 +0900

    [font] fixup script for font

--- www.jxck.io/assets/font/font.rb
+++ www.jxck.io/assets/font/font.rb
@@ -1,5 +1,12 @@
-puts Dir.glob("./**/*.md").select{|file|
-  file != "./2016-03-14/web-font-noto-sans.md"
+#!/usr/bin/env ruby
+# このスクリプトを実行すると、現時点のエントリの中から All.txt に入ってない文字だけを出す。
+# この出力を All.txt に入れて、フォントを生成し直し、バージョンをつけてデプロイする。
+current = Dir.glob("../../../blog.jxck.io/entries/**/*.md").select{|file|
+  file != "../../../blog.jxck.io/entries/2016-03-14/web-font-noto-sans.md"
 }.map{|file|
   File.read(file).gsub(/(.)/, "\\1\n")
-}.join("").split("\n").sort.uniq.join("\n")
+}.join("").split("\n").sort.uniq #.join("\n")
+
+all = File.read("./All.txt").split("\n")
+
+puts (current - all - ["", " ", "\t"])
