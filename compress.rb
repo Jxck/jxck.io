9e254c61366af0c9f544cd35f11c1fbb3f6870d5
Jxck <block.rxckin.beats@gmail.com>
2017-01-12 23:51:16 +0900

    [compress.rb] fixup compress

--- compress.rb
+++ compress.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
 ARGV.each do |dir|
-  rm = "find ./#{dir}/* -name *.gz | xargs -L 4 -P 4 rm"
+  rm = "find ./#{dir}/* -name *.gz | sed -E 's/(.*)/\"\\1\"/' | xargs -L 4 -P 4 rm"
   puts rm
   puts system(rm)
 
-  zopfli = "find ./#{dir}/* -type f | egrep -v '.webp|.rb|.otf|.woff|pdf.js|main.js' | xargs -L 1 -P 4 zopfli --i30"
+  zopfli = "find ./#{dir}/* -type f | sed -E 's/(.*)/\"\\1\"/' | egrep -v '.webp|.rb|.otf|.woff|pdf.js|main.js' | xargs -L 1 -P 4 zopfli --i30"
   puts zopfli
   puts system(zopfli)
 end
