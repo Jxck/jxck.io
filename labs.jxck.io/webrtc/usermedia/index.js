660303a956ae1931b3d31f4fd49fa9f21d4f7272
Jxck <block.rxckin.beats@gmail.com>
2017-02-02 01:19:53 +0900

    [labs] replace test to index

--- labs.jxck.io/webrtc/usermedia/index.js
+++ labs.jxck.io/webrtc/usermedia/index.js
@@ -1,255 +1,400 @@
-'use strict';
 const log = console.log.bind(console);
-const table = console.table !== undefined ? console.table.bind(console) : log;
-const dir = console.dir !== undefined ? console.dir.bind(console) : log;
-const $ = document.querySelector.bind(document);
-const j = JSON.stringify.bind(JSON);
-const p = JSON.parse.bind(JSON);
 
-// polyfill
-navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia || function(conf) {
-  navigator.getUserMedia = navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia;
-  return new Promise((resolve, reject) => {
-    navigator.getUserMedia(conf, resolve, reject);
-  });
-};
+/***********************************
+ * Stream
+ ***********************************/
 
-/**
- * basic stream info
- */
-function streamInfo(stream) {
-  $('.stream .active').textContent = stream.active;
-  $('.stream .id').textContent  = stream.id;
-  $('.tracks tbody').innerHTML = '';
-  $('.constraints tbody').innerHTML = '';
-  $('.devices tbody').innerHTML = '';
+// class
+class Track {
+  constructor(track) {
+    this.track = track
+  }
 
-  ['onaddtrack', 'onremovetrack'].forEach((ev) => {
-    stream[ev] = (e) => {
-      log('[stream]', e.type, e);
-      streamInfo(stream);
+  getConstraints() {
+    const support = !!MediaStreamTrack.prototype.getConstraints;
+    return support ? this.track.getConstraints() : {};
+  }
+
+  getCapabilities() {
+    const support = !!MediaStreamTrack.prototype.getCapabilities;
+    return support ? this.track.getCapabilities() : {};
+  }
+
+  getSettings() {
+    const support = !!MediaStreamTrack.prototype.getSettings;
+    return support ? this.track.getSettings() : {};
+  }
+}
+
+class Stream {
+  static getUserMedia(constraints) {
+    // polyfill
+    navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia || function(conf) {
+      navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+      return new Promise((resolve, reject) => {
+        navigator.getUserMedia(conf, resolve, reject);
+      });
     };
-  });
-
-  let tracks = stream.getTracks();
-  tracks.forEach((track) => {
-    // display infomations
-    trackInfo(track);
-    getConstraints(track);
-    getCapabilities(track);
-    getSettings(track);
-
-    // mute/unmute is uncontrollable, switch by device
-    ['onmute', 'onunmute', 'onended'].forEach((ev) => {
-      track[ev] = (e) => {
-        log('[track]', e.type, e);
-        streamInfo($('video.local').stream);
-      };
+    return navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+      return Promise.resolve(new Stream(stream));
     });
+  }
+
+  constructor(stream) {
+    this.stream = stream
+  }
+
+  get src() {
+    return URL.createObjectURL(this.stream);
+  }
+
+  tracks() {
+    return this.stream
+      .getTracks()
+      .map((track) => new Track(track))
+  }
+}
+
+// Action
+const getStream = (dispatch) => {
+  const constraints = {
+    audio: true,
+    video: {facingMode: 'user'},
+  };
+  Stream.getUserMedia(constraints).then((stream) => {
+    // resolve stream
+    dispatch({
+      type: 'GET_STREAM',
+      stream: stream,
+    })
   });
 }
 
-/**
- * basic track info
- */
-function trackInfo(track) {
-  let $info = [
-    track.kind,
-    track.id,
-    track.label,
-    track.enabled,
-    track.muted,
-    track.readyState,
-  ].map((v) => {
-    let $td = document.createElement('td');
-    $td.textContent = v;
-    return $td;
-  }).reduce(($tr, $td) => {
-    $tr.appendChild($td);
-    return $tr;
-  }, document.createElement('tr'));
-  $('.tracks tbody').appendChild($info);
+// Reducer
+const streamReducer = (state = {stream: undefined}, action) => {
+  switch (action.type) {
+    case 'GET_STREAM':
+      return Object.assign({}, state, {
+        stream: action.stream,
+      })
+    default:
+      return state
+  }
 }
 
-/**
- * getConstraints()
- */
-function getConstraints(track) {
-  if (track.getConstraints === undefined) {
-    $('.getConstraints').innerHTML = 'not supported';
-    return
+// Components
+class Tracks extends React.Component {
+  render() {
+    const { tracks } = this.props;
+    const tr = tracks.map((track) => {
+      return (
+        <tr>
+          <td>{track.kind}</td>
+          <td>{track.id}</td>
+          <td>{track.label}</td>
+          <td>{track.enabled}</td>
+          <td>{track.muted}</td>
+          <td>{track.readyState}</td>
+        </tr>
+      )
+    });
+
+    return (
+      <table>
+        <tr>
+          <th>kind</th>
+          <th>id</th>
+          <th>label</th>
+          <th>enabled</th>
+          <th>muted</th>
+          <th>readyState</th>
+        </tr>
+        {tr}
+      </table>
+    )
   }
-
-  let keys = p($('.constraints').dataset.keys);
-  let constraints = track.getConstraints();
-
-  let $tr = keys.map((key, i) => {
-    if (i === 0) return `${track.kind} constraints`;
-    return j(constraints[key]);
-  }).map((v) => {
-    let $td = document.createElement('td');
-    $td.textContent = v;
-    return $td;
-  }).reduce(($tr, $td) => {
-    $tr.appendChild($td);
-    return $tr;
-  }, document.createElement('tr'));
-
-  $('.constraints tbody').appendChild($tr);
 }
 
-/**
- * getSettings()
- */
-function getSettings(track) {
-  if (track.getSettings === undefined) {
-    $('.getSettings').innerHTML = 'not supported';
-    return
+class Video extends React.Component {
+  render() {
+    const { stream } = this.props;
+    if (stream === undefined) {
+      return <p>empty</p>
+    }
+
+    return (
+      <div>
+        <p>stream.active: {stream.active ? 'active': 'innactive'}</p>
+        <p>stream.id: {stream.id}</p>
+        <video autoPlay controls src={stream.src}></video>
+      </div>
+    )
   }
-
-  let keys = p($('.constraints').dataset.keys);
-  let settings = track.getSettings();
-
-  let $tr = keys.map((key, i) => {
-    if (i === 0) return `${track.kind} settings`;
-    return j(settings[key]);
-  }).map((v) => {
-    let $td = document.createElement('td');
-    $td.textContent = v;
-    return $td;
-  }).reduce(($tr, $td) => {
-    $tr.appendChild($td);
-    return $tr;
-  }, document.createElement('tr'));
-
-  $('.constraints tbody').appendChild($tr);
 }
 
-/**
- * getCapabilities()
- */
-function getCapabilities(track) {
-  if (track.getCapabilities === undefined) {
-    $('.getCapabilities').innerHTML = 'not supported';
-    return
+// Components
+class StreamComponent extends React.Component {
+  componentDidMount() {
+    // TODO: remove
+    // this.props.getStream();
   }
 
-  let keys = p($('.constraints').dataset.keys);
-  let capabilities = track.getCapabilities();
-
-  let $tr = keys.map((key, i) => {
-    if (i === 0) return `${track.kind} capabilities`;
-    return j(capabilities[key]);
-  }).map((v) => {
-    let $td = document.createElement('td');
-    $td.textContent = v;
-    return $td;
-  }).reduce(($tr, $td) => {
-    $tr.appendChild($td);
-    return $tr;
-  }, document.createElement('tr'));
-
-  $('.constraints tbody').appendChild($tr);
+  render() {
+    const { stream, getStream } = this.props;
+    return (
+      <section>
+        <h2>Stream</h2>
+        <Video stream={stream} />
+        <Tracks tracks={stream ? stream.tracks() : []} />
+        <button onClick={getStream}>start</button>
+      </section>
+    )
+  }
 }
 
-/**
- * getSupportedConstraints()
- */
-function getSupportedConstraints() {
-  if (navigator.mediaDevices.getSupportedConstraints === undefined) {
-    $('.getSupportedConstraints').innerHTML = 'not supported';
-    return
+// Container
+const StreamContainer = ReactRedux.connect(
+  (state) => {
+    return {
+      stream: state.stream.stream,
+    }
+  },
+  (dispatch) => {
+    return {
+      getStream() {
+        getStream(dispatch)
+      }
+    }
   }
+)(StreamComponent);
 
-  const constraints = navigator.mediaDevices.getSupportedConstraints();
 
-  // sortedkeys
-  let keys = Object.keys(constraints).sort((a, b) => {
+
+
+/***********************************
+ * Constraints
+ ***********************************/
+
+// Action
+const getSupportedConstraints = (dispatch) => {
+  const keys = navigator.mediaDevices.getSupportedConstraints();
+  let supported = Object.keys(keys).sort((a, b) => {
     if (a.startsWith('moz')) return 1;
     return (a.length > b.length) ? 1 : -1;
   });
-  keys.unshift(''); // for method
-  $('.constraints').dataset.keys = j(keys);
-
-  // th line
-  let $tr = keys
-    .map((c) => {
-      let $th = document.createElement('th');
-      $th.textContent = c;
-      return $th;
-    }).reduce(($tr, $th) => {
-      $tr.appendChild($th);
-      return $tr;
-    }, document.createElement('tr'));
-  $('.constraints thead').appendChild($tr);
+  dispatch({
+    type: 'SUPPORTED_CONSTRAINTS',
+    supported, supported,
+  })
 }
 
-/**
- * enumerateDevices()
- */
-function enumerateDevices() {
-  navigator.mediaDevices.ondevicechange = (e) => {
-    log('[devices]', e.type, e);
-    enumerateDevices();
-  };
+// Reducer
+const constraintsReducer = (state = {supported: []}, action) => {
+  switch (action.type) {
+    case 'SUPPORTED_CONSTRAINTS':
+      return Object.assign({}, state, {
+        supported: action.supported
+      })
+    default:
+      return state
+  }
+}
+
+// Components
+class Constraints extends React.Component {
+  componentDidMount() {
+    this.props.getSupportedConstraints();
+  }
+
+  render() {
+    const { supported, tracks } = this.props;
+    const th = supported.map((key) => <th>{key}</th>)
+
+    const consts = tracks.map((track) => {
+      const values = track.getConstraints();
+      const td = supported
+        .map((key) => values[key])
+        .map((value) => <td>{JSON.stringify(value)}</td>);
+
+      return <tr><td>constraints</td>{td}</tr>;
+    });
+
+    const caps = tracks.map((track) => {
+      const values = track.getCapabilities();
+      const td = supported
+        .map((key) => values[key])
+        .map((value) => <td>{JSON.stringify(value)}</td>);
+
+      return <tr><td>capabilities</td>{td}</tr>;
+    });
+
+    const settings = tracks.map((track) => {
+      const values = track.getSettings();
+      const td = supported
+        .map((key) => values[key])
+        .map((value) => <td>{JSON.stringify(value)}</td>);
+
+      return <tr><td>settings</td>{td}</tr>;
+    });
+
+    return (
+      <section>
+        <h2>Constraints</h2>
+        <table>
+          <tr>{th}</tr>
+          {consts}
+          {caps}
+          {settings}
+        </table>
+      </section>
+    )
+  }
+}
+
+// Container
+const ConstraintsContainer = ReactRedux.connect(
+  (state) => {
+    const stream = state.stream.stream;
+    return {
+      supported: state.constraints.supported,
+      tracks: stream ? stream.tracks() : [],
+    }
+  },
+  (dispatch) => {
+    return {
+      getSupportedConstraints() {
+        getSupportedConstraints(dispatch)
+      }
+    }
+  }
+)(Constraints);
+
+
+
+
+/***********************************
+ * Device
+ ***********************************/
+
+// Action
+const enumDevice = (dispatch) => {
   navigator.mediaDevices.enumerateDevices().then((devices) => {
-    devices.map((device) => {
-      return [
-        device.kind,
-        device.label,
-        device.deviceId,
-        device.groupId
-      ];
-    }).map((devices) => {
-      return devices.map((device) => {
-        let $td = document.createElement('td');
-        $td.textContent = device;
-        return $td;
-      }).reduce(($tr, $td) => {
-        $tr.appendChild($td);
-        return $tr;
-      }, document.createElement('tr'));
-    }).forEach(($tr) => {
-      $('.devices tbody').appendChild($tr);
+    dispatch({
+      type: 'ENUM_DEVICE',
+      devices: devices.sort((a, b) => a.kind > b.kind ? 1: -1),
     });
   });
+
+  navigator.mediaDevices.ondevicechange = (e) => {
+    enumDevice(dispatch);
+  };
 }
 
-/**
- * main()
- */
-document.addEventListener('DOMContentLoaded', (a) => {
-  enumerateDevices();
-  getSupportedConstraints();
-
-
-  $('button.start').addEventListener('click', () => {
-    let value = $('[name=facingMode]').value;
-    const constraints = {
-      audio: true,
-      video: {facingMode: value}
-    };
-
-    navigator
-      .mediaDevices
-      .getUserMedia(constraints)
-      .then((stream) => {
-        streamInfo(stream);
-        $('video.local').src = URL.createObjectURL(stream);
-        $('video.local').stream = stream;
-
-        $('button.stop').addEventListener('click', () => {
-          stream.getTracks().forEach((track) => {
-            log('[track]', 'stop', track.kind, track.id, track.label);
-            track.stop();
-          });
-        });
+// Reducer
+const deviceReducer = (state = {devices: []}, action) => {
+  switch (action.type) {
+    case 'ENUM_DEVICE':
+      return Object.assign({}, state, {
+        devices: action.devices
       })
-      .catch(log);
-  });
+    default:
+      return state
+  }
+}
 
-  // $('button.start').dispatchEvent(new Event('click'));
+// Component
+class Device extends React.Component {
+  componentDidMount() {
+    this.props.onEnumDevice();
+  }
+
+  render() {
+    const {onEnumDevice, devices} = this.props
+    const tr = devices.map((d) => {
+      return (
+        <tr>
+          <td>{d.kind}</td>
+          <td>{d.label}</td>
+          <td>{d.deviceId}</td>
+          <td>{d.groupId}</td>
+        </tr>
+      )
+    });
+    return (
+      <section>
+        <h2>Devices</h2>
+        <button onClick={onEnumDevice}>refresh</button>
+        <table>
+          <tr>
+            <th>kind</th>
+            <th>label</th>
+            <th>deviceId</th>
+            <th>groupId</th>
+          </tr>
+          {tr}
+        </table>
+      </section>
+    )
+  }
+}
+
+// Container
+const DeviceContainer = ReactRedux.connect(
+  (state) => {
+    return {
+      devices: state.device.devices,
+    }
+  },
+  (dispatch) => {
+    return {
+      onEnumDevice() {
+        enumDevice(dispatch)
+      }
+    }
+  }
+)(Device);
+
+
+/**
+ * App
+ */
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <StreamContainer />
+        <ConstraintsContainer />
+        <DeviceContainer />
+      </div>
+    )
+  }
+}
+
+const AppContainer = ReactRedux.connect(
+  (state) => {
+    return {
+    }
+  },
+  (dispatch) => {
+    return {
+    }
+  }
+)(App);
+
+
+const reducer = Redux.combineReducers({
+  device: deviceReducer,
+  stream: streamReducer,
+  constraints: constraintsReducer,
 });
+const store = Redux.createStore(reducer)
+
+ReactDOM.render(
+  <ReactRedux.Provider store={store}>
+    <AppContainer />
+  </ReactRedux.Provider>,
+  document.getElementById('root')
+)
