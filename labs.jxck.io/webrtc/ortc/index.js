0d0fcf1e3dabe1825b226aebe0b028aa2b43de36
Jxck <block.rxckin.beats@gmail.com>
2017-02-25 04:42:45 +0000

    [labs] replace ws.js in ortc

--- labs.jxck.io/webrtc/ortc/index.js
+++ labs.jxck.io/webrtc/ortc/index.js
@@ -1,3 +1,50 @@
+debug = DEBUG ? console.debug.bind(console) : ()=>{}
+
+class WS extends EventEmitter {
+  constructor(url, protocols) {
+    super()
+
+    this.id = btoa(Math.random()*1000)
+
+    this.ws = new WebSocket(url, protocols)
+
+    this.ws.onopen = (e) => {
+      debug(`ws#on('${e.type}')`, e, this.id)
+      super.emit('open', e)
+    }
+
+    this.ws.onmessage = (e) => {
+      const {id, name, message} = JSON.parse(e.data)
+      if (id === this.id) return;
+      debug(`ws#on('${name}')`, message)
+      super.emit(name, message)
+    }
+
+    this.ws.onclose = (e) => {
+      debug(`ws#on('${e.type}')`, e)
+      super.emit('close', e)
+    }
+
+    this.ws.onerror = (e) => {
+      debug(`ws#on('${e.type}')`, e)
+      super.emit('error', e)
+    }
+  }
+
+  emit(name, message) {
+    console.assert(name !== undefined && message !== undefined)
+    debug(`ws#send(name, data)`, name, message)
+    this.ws.send(JSON.stringify({id: this.id, name, message}))
+  }
+
+  close(code, reason) {
+    debug(`ws#close(code, reason)`, code, reason)
+    this.ws.close(code, reason)
+  }
+}
+
+
+
 var util = {};
 util.Caps2Params = function(sendCaps, remoteRecvCaps) {
   let muxId = '';
@@ -86,7 +133,7 @@ util.RTCRtpEncodingParameters = function(ssrc, codecPayloadType, fec, rtx, prior
 (function(global) {
   'use strict';
   let id = location.hash;
-  let socket = new Socket();
+  let socket = new WS('wss://ws.jxck.io', ['broadcast', 'ortc-demo']);
 
   let iceOptions = { gatherPolicy: 'all', iceServers: [] };
 
