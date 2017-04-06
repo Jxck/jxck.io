d4302155fd2895748c5536aae767bcec9931275f
Jxck <block.rxckin.beats@gmail.com>
2017-04-06 09:58:31 +0900

    [labs] fixup gen_tcp

--- labs.jxck.io/erlang/gen_tcp/client.erl
+++ labs.jxck.io/erlang/gen_tcp/client.erl
@@ -7,19 +7,31 @@
 
 -include("../logger.hrl").
 
+%% サーバからの受信なので {active, true} で開く。
+%% 受信と送信が平行できるように受信は spawn し、制御プロセスを移譲する。
 main(_) ->
     {ok, Socket} = ?Log(gen_tcp:connect({127, 0, 0, 1}, 3000, [binary, {active, true}])),
-    ?Log(gen_tcp:send(Socket, "foo")),
-    loop(Socket).
 
+    PID = spawn(fun() -> receive_loop(Socket) end),
+    gen_tcp:controlling_process(Socket, PID),
 
-loop(Socket) ->
+    send_loop(Socket).
+
+%% コンソールから入力を受け取り送信
+send_loop(Socket) ->
+    {ok, [Msg]} = io:fread("> ", "~s"),
+    gen_tcp:send(Socket, Msg),
+    send_loop(Socket).
+
+%% 受信
+receive_loop(Socket) ->
     receive
         {tcp, Socket, Data} ->
             ?Log(Data),
-            ?Log(gen_tcp:close(Socket));
+            receive_loop(Socket);
         {tcp_closed, Socket} ->
-            ?Log({tcp_closed, Socket});
+            ?Log({tcp_closed, Socket}),
+            ?Log(gen_tcp:close(Socket));
         Error ->
             ?Log(Error)
     end.
--- labs.jxck.io/erlang/gen_tcp/server.erl
+++ labs.jxck.io/erlang/gen_tcp/server.erl
@@ -7,21 +7,33 @@
 
 -include("../logger.hrl").
 
+%% {active, true} : 何もしなくてもメッセージ上がる、流量の制限ができないので DOS の心配がある
+%% {active, false}: gen_tcp:recv() しないと、メッセージを取しない
+%% {active, once} : 1 つだけ受信し、処理が終わってから再度 inet:setopts すればまたメッセージが上がる
 main(_) ->
     {ok, Listen} = ?Log(gen_tcp:listen(3000, [{reuseaddr, true}, {active, once}, binary])),
+    accept_loop(Listen).
+
+%% 接続待ちのループ
+%% このプロセスで accept した socket へのメッセージは
+%% このプロセスに来てしまうので、制御プロセスを移譲する。
+accept_loop(Listen) ->
     {ok, Socket} = ?Log(gen_tcp:accept(Listen)),
-    loop(Socket).
+    PID = spawn(fun() -> receive_loop(Socket) end),
+    gen_tcp:controlling_process(Socket, PID),
+    accept_loop(Listen).
 
-
-loop(Socket) ->
+%% 単一のソケットを制御するループ
+receive_loop(Socket) ->
     receive
         {tcp, Socket, Data} ->
+            ?Log(Data, from, Socket),
             gen_tcp:send(Socket, Data),
-            ?Log(Data),
             inet:setopts(Socket, [{active, once}]),
-            loop(Socket);
+            receive_loop(Socket);
         {tcp_closed, Socket} ->
-            ?Log({tcp_closed, Socket});
+            ?Log({tcp_closed, Socket}),
+            ?Log(gen_tcp:close(Socket));
         Error ->
             ?Log(Error)
     end.
