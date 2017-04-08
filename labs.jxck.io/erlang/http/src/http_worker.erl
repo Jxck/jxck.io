c81294e10b25037500409db4f723cf8375156464
Jxck <block.rxckin.beats@gmail.com>
2017-04-08 15:35:51 +0900

    [labs] add http handler

--- labs.jxck.io/erlang/http/src/http_worker.erl
+++ labs.jxck.io/erlang/http/src/http_worker.erl
@@ -10,6 +10,8 @@
 %% Supervisor callbacks
 -export([start_link/1]).
 
+-export([get/1]).
+
 %%====================================================================
 %% API functions
 %%====================================================================
@@ -23,25 +25,34 @@ start_link(Socket)->
 %% Internal functions
 %%====================================================================
 
+default(#{body := Body, headers := _Headers}=Response) ->
+    DefaultHeader = #{
+      "Date" => httpd_util:rfc1123_date(),
+      "Content-Length" => integer_to_list(byte_size(Body))
+     },
+    Headers = maps:merge(DefaultHeader, _Headers),
+    maps:put(headers, Headers, Response).
+
+
 process(Socket, Size, Timeout) ->
     case (gen_tcp:recv(Socket, Size, Timeout)) of
         {ok, Packet} ->
-            Response = response(Packet),
-            gen_tcp:send(Socket, Response),
+            {ok, #{method := Method, target := URL, headers := Header, body := Body}} = http:decode(Packet),
+            Fn = list_to_atom(string:to_lower(Method)),
+            Response = apply(?MODULE, Fn, [{URL, Header, Body}]),
+            gen_tcp:send(Socket, http:encode(default(Response))),
             gen_tcp:close(Socket);
         {error, Reason} ->
             ?Log("failed to recv:", Reason),
             gen_tcp:close(Socket)
     end.
 
-response(Request) ->
-    ?Log(http:decode(Request)),
-    <<
-      "HTTP/1.0 200 OK\r\n",
-      "Date: Tue, 25 Oct 2016 10:21:33 GMT\r\n",
-      "Connection: close\r\n",
-      "Content-Type: text/plain; charset=utf-8\r\n",
-      "Content-Length: 11\r\n",
-      "\r\n",
-      "hello world"
-    >>.
+get({URL, Header, Body}) ->
+    #{ status  => "200",
+       headers => #{
+         "Content-Type" => "text/html; charset=utf-8",
+         "Connection" => "close",
+         "Server" => "erlang"
+        },
+       body => <<"<h1>hello world</h1>">>
+     }.
