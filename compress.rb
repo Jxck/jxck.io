de09ffa4c0aec111fe3a8a04cac0edf6f55a23a2
Jxck <block.rxckin.beats@gmail.com>
2017-08-07 16:38:51 +0900

    [build] support brotli in compress.br

--- compress.rb
+++ compress.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/env ruby
 
 ARGV.each do |dir|
-  rm = "find ./#{dir}/* -name *.gz | sed -E 's/(.*)/\"\\1\"/' | xargs -L 4 -P 4 rm"
+  rm = "selects path from './#{dir}/**/*' where extname '=~' 'gz|br' | sed -E 's/(.*)/\"\\1\"/' | xargs -P 4 rm"
   puts rm
   puts system(rm)
 
-  zopfli = "find ./#{dir}/* -type f | sed -E 's/(.*)/\"\\1\"/' | egrep -v '.webp|.rb|.otf|.woff|pdf.js|main.js' | xargs -L 1 -P 4 zopfli --i30"
+  zopfli = "find ./#{dir}/* -type f | sed -E 's/(.*)/\"\\1\"/' | egrep -v '.gz|.br|.webp|.rb|.otf|.woff|pdf.js|main.js' | xargs -P 4 zopfli --i30"
   puts zopfli
   puts system(zopfli)
+
+  brotli = "find ./#{dir}/* -type f | sed -E 's/(.*)/\"\\1\"/' | egrep -v '.gz|.br|.webp|.rb|.otf|.woff|pdf.js|main.js' | xargs -P 4 -IXXX bro --quality 10 --input XXX --output XXX.br"
+  puts brotli
+  puts system(brotli)
 end
