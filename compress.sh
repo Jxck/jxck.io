f688429561983f598767eb95603ee40454acbc04
Jxck <block.rxckin.beats@gmail.com>
2018-02-18 16:13:12 +0900

    [compress] fixup compress.sh

--- compress.sh
+++ compress.sh
@@ -8,10 +8,10 @@ do
 
   echo
   echo "zopfli ${1}"
-  selects path from "./${1}/**/*" where path '!~' 'drafts' and extname '=~' '[^.gz|.br|.webp|.rb|.md|.woff2]' | q | xargs -P 4 zopfli --i30
+  selects path from "./${1}/**/*" where path '!~' 'drafts' and extname '=~' '[^.gz|.br|.webp|.rb|.md|.woff2]' | q | tee -a /dev/stderr | xargs -P 4 zopfli --i30
 
   echo
   echo "brotli ${1}"
-  selects path from "./${1}/**/*" where path '!~' 'drafts' and extname '=~' '[^.gz|.br|.webp|.rb|.md|.woff2]' | q | xargs -P 4 -IXXX brotli -v XXX
+  selects path from "./${1}/**/*" where path '!~' 'drafts' and extname '=~' '[^.gz|.br|.webp|.rb|.md|.woff2]' | q | tee -a /dev/stderr | xargs -P 4 -IXXX brotli XXX
   shift
 done
