f84bb1e2d49b7171dc726effbe28bd6502ad7f84
Jxck <block.rxckin.beats@gmail.com>
2018-03-17 00:46:17 +0900

    [admin] use wildcard certificate by ACME v2

--- admin.jxck.io/cert-jxck.sh
+++ admin.jxck.io/cert-jxck.sh
@@ -1,25 +1,37 @@
 certbot-auto certonly \
-  -n \
-  --webroot \
-  --agree-tos \
+  --manual \
   --email admin@jxck.io \
-  -w ../www.jxck.io \
-  --expand \
+  --agree-tos \
   -d jxck.io \
-  -d www.jxck.io \
-  -d blog.jxck.io \
-  -d files.jxck.io \
-  -d slide.jxck.io \
-  -d admin.jxck.io \
-  -d ws.jxck.io \
-  -d rails.jxck.io \
-  -d api.jxck.io \
-  -d report-uri.jxck.io \
-  -d sfu.jxck.io \
-  -d spec.jxck.io \
-  -d logo.jxck.io \
-  -d labs.jxck.io \
-  -d lab2.jxck.io
+  -d *.jxck.io \
+  --manual-public-ip-logging-ok \
+  --preferred-challenges dns-01 \
+  --server https://acme-v02.api.letsencrypt.org/directory
+
+
+
+# certbot-auto certonly \
+#   -n \
+#   --webroot \
+#   --agree-tos \
+#   --email admin@jxck.io \
+#   -w ../www.jxck.io \
+#   --expand \
+#   -d jxck.io \
+#   -d www.jxck.io \
+#   -d blog.jxck.io \
+#   -d files.jxck.io \
+#   -d slide.jxck.io \
+#   -d admin.jxck.io \
+#   -d ws.jxck.io \
+#   -d rails.jxck.io \
+#   -d api.jxck.io \
+#   -d report-uri.jxck.io \
+#   -d sfu.jxck.io \
+#   -d spec.jxck.io \
+#   -d logo.jxck.io \
+#   -d labs.jxck.io \
+#   -d lab2.jxck.io
 
 # no https
 # turn.jxck.io
