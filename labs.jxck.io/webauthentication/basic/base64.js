685cafe923be73acdfa40e7a6c05b82a786aff76
Jxck <block.rxckin.beats@gmail.com>
2018-05-05 00:36:16 +0900

    [labs] add base64 to webauthn

--- labs.jxck.io/webauthentication/basic/base64.js
+++ labs.jxck.io/webauthentication/basic/base64.js
@@ -1,8 +1,17 @@
 'use strict';
 
-// TODO: make me es module if https://crbug.com/680046 fixed
+// alias
+function b64uenc(bin) {
+  return b64enc(bin, {urlsafe:true})
+}
 
-function b64enc(uint8array, opt={urlsafe:true}) {
+function b64udec(b64str) {
+  return b64dec(b64str, {urlsafe:true})
+}
+
+// TODO: make me es module if https://crbug.com/680046 fixed
+function b64enc(bin, opt={urlsafe:true}) {
+  const uint8array = new Uint8Array(bin)
   const str = btoa(String.fromCharCode(...uint8array))
   if (opt.urlsafe) {
     return str
