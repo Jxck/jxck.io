a7b2fcd898fde0ca91b9418e8ef648beb2c6fb1e
Jxck <block.rxckin.beats@gmail.com>
2018-06-01 00:21:18 +0900

    [labs] update webpackage

--- labs.jxck.io/webpackage/sign.sh
+++ labs.jxck.io/webpackage/sign.sh
@@ -1,15 +1,16 @@
-go get github.com/nyaxt/webpackage/go/signedexchange/cmd/gen-certurl
-go get github.com/nyaxt/webpackage/go/signedexchange/cmd/gen-signedexchange
-mv ./nyaxt/bin/* .
-rm -rf ./nyaxt
+# go get github.com/nyaxt/webpackage/go/signedexchange/cmd/gen-certurl
+# go get github.com/nyaxt/webpackage/go/signedexchange/cmd/gen-signedexchange
+# mv ./nyaxt/bin/* .
+# rm -rf ./nyaxt
 
 ./gen-certurl /etc/letsencrypt/live/mozaic.fm/cert.pem  > cert.pem.msg
 
 ./gen-signedexchange \
   -uri https://mozaic.fm/index.html \
-  -content ./index.html \
+  -content /home/jxck/server/jxck.io/mozaic.fm/index.html \
   -certificate /etc/letsencrypt/live/mozaic.fm/cert.pem \
   -certUrl https://labs.jxck.io/webpackage/cert.pem.msg \
   -validityUrl https://labs.jxck.io/webpackage/cert.pem.msg \
   -privateKey /etc/letsencrypt/live/mozaic.fm/privkey.pem \
+  -expire 24h0m0s \
   -o ./mozaic.sxg
