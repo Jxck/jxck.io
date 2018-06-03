f2e1d4fc230a25c9a6d61032db2ed163b6f9b104
Jxck <block.rxckin.beats@gmail.com>
2018-06-03 16:01:11 +0900

    [labs] move webpackaging

--- labs.jxck.io/webpackaging/signed-http-exchange/sign.sh
+++ labs.jxck.io/webpackaging/signed-http-exchange/sign.sh
@@ -9,8 +9,8 @@
   -uri https://mozaic.fm/index.html \
   -content /home/jxck/server/jxck.io/mozaic.fm/index.html \
   -certificate /etc/letsencrypt/live/mozaic.fm/cert.pem \
-  -certUrl https://labs.jxck.io/webpackage/cert.pem.msg \
-  -validityUrl https://labs.jxck.io/webpackage/cert.pem.msg \
+  -certUrl https://labs.jxck.io/webpackaging/signed-http-exchange/cert.pem.msg \
+  -validityUrl https://labs.jxck.io/webpackaging/signed-http-exchange/cert.pem.msg \
   -privateKey /etc/letsencrypt/live/mozaic.fm/privkey.pem \
   -expire 24h0m0s \
   -o ./mozaic.sxg
