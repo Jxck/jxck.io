e8b187b341ba9ff09fc4781c04557f8d456cbe34
Jxck <block.rxckin.beats@gmail.com>
2018-07-31 17:24:00 +0900

    [js] add ReportingObserver to main.js/mozaic.js

--- www.jxck.io/assets/js/main.js
+++ www.jxck.io/assets/js/main.js
@@ -3,3 +3,12 @@ console.log(`貴様、見ているな！！
 https://labs.jxck.io/blog
 by Jxck
 `)
+
+if (window.ReportingObserver) {
+  (new ReportingObserver((reports, observer) => {
+    reports.map((report) => {
+      navigator.sendBeacon("https://report-uri.jxck.io/report-to.cgi", JSON.stringify(report))
+    })
+    console.log(reports)
+  })).observe()
+}
--- www.jxck.io/assets/js/mozaic.js
+++ www.jxck.io/assets/js/mozaic.js
@@ -1,7 +1,14 @@
-'use strict';
-
 console.log(`貴様、見ているな！！
 このサイトの技術的なまとめはこちらをどうぞ。
 https://labs.jxck.io/blog
 by Jxck
 `)
+
+if (window.ReportingObserver) {
+  (new ReportingObserver((reports, observer) => {
+    reports.map((report) => {
+      navigator.sendBeacon("https://report-uri.jxck.io/report-to.cgi", JSON.stringify(report))
+    })
+    console.log(reports)
+  })).observe()
+}
