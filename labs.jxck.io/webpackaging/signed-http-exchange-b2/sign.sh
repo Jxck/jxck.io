e0245ff8d7cb36c170d8dec4e7d3ea5931ffba52
Jxck <block.rxckin.beats@gmail.com>
2018-11-27 01:32:45 +0900

    [labs] up Signed HTTP Exchange v=b2 DEMO

--- labs.jxck.io/webpackaging/signed-http-exchange-b2/sign.sh
+++ labs.jxck.io/webpackaging/signed-http-exchange-b2/sign.sh
@@ -1,19 +1,17 @@
-go get -u github.com/WICG/webpackage/go/signedexchange/cmd/gen-certurl
-go get -u github.com/WICG/webpackage/go/signedexchange/cmd/gen-signedexchange
-go get -u github.com/WICG/webpackage/go/signedexchange/cmd/dump-certurl
-go get -u github.com/WICG/webpackage/go/signedexchange/cmd/dump-signedexchange
-
-
-echo "<h1>hi</h1>" > payload.html
-
-# Generate prime256v1 ecdsa private key.
-openssl ecparam -out priv.key -name prime256v1 -genkey
-
-# Create a certificate signing request for the private key.
-openssl req -new -sha256 -key priv.key -out cert.csr -subj '/CN=mozaic.fm/O=Test/C=US'
-
-# Self-sign the certificate with "CanSignHttpExchanges" extension.
-openssl x509 -req -days 360 -in cert.csr -signkey priv.key -out cert.pem
+#  go get -u github.com/WICG/webpackage/go/signedexchange/cmd/gen-certurl
+#  go get -u github.com/WICG/webpackage/go/signedexchange/cmd/gen-signedexchange
+#  go get -u github.com/WICG/webpackage/go/signedexchange/cmd/dump-certurl
+#  go get -u github.com/WICG/webpackage/go/signedexchange/cmd/dump-signedexchange
+#
+#
+# # Generate prime256v1 ecdsa private key.
+# openssl ecparam -out priv.key -name prime256v1 -genkey
+#
+# # Create a certificate signing request for the private key.
+# openssl req -new -sha256 -key priv.key -out cert.csr -subj '/CN=mozaic.fm/O=Test/C=US'
+#
+# # Self-sign the certificate with "CanSignHttpExchanges" extension.
+# openssl x509 -req -days 360 -in cert.csr -signkey priv.key -out cert.pem
 
 # dummy file
 echo ocsp > tmp
@@ -22,13 +20,13 @@ echo ocsp > tmp
 ./bin/gen-certurl -pem cert.pem -ocsp tmp > cert.cbor
 
 ./bin/gen-signedexchange \
-  -uri         https://mozaic.fm/hello.html \
-  -content     ./payload.html \
+  -uri         https://mozaic.fm/index.html \
+  -content     ../../../mozaic.fm/index.html \
   -certificate cert.pem \
   -privateKey  priv.key \
-  -certUrl     https://labs.jxck.io/webpackaging/tmp/cert.cbor \
+  -certUrl     https://labs.jxck.io/webpackaging/signed-http-exchange-b2/cert.cbor \
   -validityUrl https://mozaic.fm/cert.cbor \
-  -o           hello.sxg
+  -o           mozaic.sxg
 
 
 export LIST=`openssl x509 -noout -pubkey -in cert.pem | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | base64`
