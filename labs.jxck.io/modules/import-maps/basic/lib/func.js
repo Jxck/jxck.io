168fa39a49cbee286e7db83552c91ef6e913806c
Jxck <block.rxckin.beats@gmail.com>
2019-03-09 18:29:30 +0900

    [labs] update import-maps

---
+++ labs.jxck.io/modules/import-maps/basic/lib/func.js
@@ -0,0 +1,3 @@
+export default (p1, p2) => {
+  return ((p1.x - p2.x)**2 + (p1.y - p2.y)**2)**0.5
+}
---
+++ labs.jxck.io/modules/import-maps/basic/lib/position.js
@@ -0,0 +1,6 @@
+export default class Position {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+}
