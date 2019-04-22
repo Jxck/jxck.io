50bc16f3ee831778800a68505673e035e3f22578
Jxck <block.rxckin.beats@gmail.com>
2019-04-23 00:04:21 +0900

    [mozaic] update player and search dialog

--- www.jxck.io/assets/js/mozaic.js
+++ www.jxck.io/assets/js/mozaic.js
@@ -3,9 +3,40 @@ import MozaicPlayer from '/assets/js/mozaic-player.mjs'
 
 // Enable debug log adding #debug into url
 const log = location.hash === "#debug" ? console.log.bind(console) : () => {}
-EventTarget.prototype.on = EventTarget.prototype.addEventListener
+EventTarget.prototype.on  = EventTarget.prototype.addEventListener
+EventTarget.prototype.off = EventTarget.prototype.removeEventListener
 const $ = document.querySelector.bind(document)
 
+// keybind
+function playerKeybind(e) {
+  log(e.key, e.target, document.activeElement)
+
+  if ([e.target, document.body].includes(document.activeElement) === false) return
+
+  switch(e.key) {
+    case ' ':
+      log('play/pause')
+      $('mozaic-player').play()
+      e.preventDefault()
+      break
+    case 'ArrowLeft':
+      log('back')
+      $('mozaic-player').back()
+      break
+    case 'ArrowRight':
+      log('forward')
+      $('mozaic-player').forward()
+      break
+      // case 'ArrowUp':
+      //   log('volume up')
+      //   $('mozaic-player').volumeup()
+      //   break
+      // case 'ArrowDown':
+      //   log('volume down')
+      //   $('mozaic-player').volumedown()
+      //   break
+  }
+}
 if (window.matchMedia( "(max-width: 800px)" ).matches || window.customElements === undefined) {
   // スマホの UI は デフォルトの controls が一番使いやすい気がする
   // custom element 無い場合も controls
@@ -15,36 +46,7 @@ if (window.matchMedia( "(max-width: 800px)" ).matches || window.customElements =
   log(MozaicPlayer)
   customElements.define('mozaic-player', MozaicPlayer);
 
-  // main
-  document.on('keydown', (e) => {
-    log(e.key, document.activeElement)
-
-    if ([e.target, document.body].includes(document.activeElement) === false) return
-
-    switch(e.key) {
-      case ' ':
-        log('play/pause')
-        $('mozaic-player').play()
-        e.preventDefault()
-        break
-      case 'ArrowLeft':
-        log('back')
-        $('mozaic-player').back()
-        break
-      case 'ArrowRight':
-        log('forward')
-        $('mozaic-player').forward()
-        break
-        // case 'ArrowUp':
-        //   log('volume up')
-        //   $('mozaic-player').volumeup()
-        //   break
-        // case 'ArrowDown':
-        //   log('volume down')
-        //   $('mozaic-player').volumedown()
-        //   break
-    }
-  })
+  document.on('keydown', playerKeybind)
 }
 
 if (navigator.share) {
@@ -79,8 +81,13 @@ document.on('DOMContentLoaded', (e) => {
     $('.search').on('click', (e) => {
       log(e)
       e.preventDefault()
-      const $dialog = $('.dialog')
+
+      // player が使ってるキーバインドを一旦外す
+      document.off('keydown', playerKeybind)
+
+      const $dialog = $('dialog')
       $dialog.on('click', (e) => {
+        console.log(e.target, $dialog)
         if (e.target === $dialog) {
           $dialog.close()
         }
@@ -90,6 +97,9 @@ document.on('DOMContentLoaded', (e) => {
       })
       $dialog.on('close', (e) => {
         log(e.type, $dialog.returnValue)
+
+        // player が使ってるキーバインドを戻す
+        document.on('keydown', playerKeybind)
       })
       $dialog.showModal()
     })
