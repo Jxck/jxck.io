bc284dbfdd50897aa933c58b800c3f6217d97fe6
Jxck <block.rxckin.beats@gmail.com>
2019-04-26 14:37:17 +0900

    [npm] add cleanup.sh

--- compress.sh
+++ compress.sh
@@ -3,10 +3,6 @@
 for i in `seq 1 ${#}`
 do
   echo
-  echo "rm ${1}"
-  selects path from "./${1}/**/*" where extname '=~' 'gz|br' | q | tee -a /dev/stderr | xargs -L 1 -P 4 rm
-
-  echo
   echo "zopfli ${1}"
   selects path from "./${1}/**/*" where path '!~' 'drafts' and extname '=~' '[^.gz|.br|.webp|.rb|.md|.woff2]' | q | tee -a /dev/stderr | xargs -P 4 zopfli --i30
 
