ab86e9ad9eba8beaaef9c1730c6ec82cd83e0b0f
Jxck <block.rxckin.beats@gmail.com>
2019-07-19 19:49:31 +0900

    [sw] dump version

--- www.jxck.io/assets/js/sw.js
+++ www.jxck.io/assets/js/sw.js
@@ -3,7 +3,7 @@
   EventTarget.prototype.on  = EventTarget.prototype.addEventListener
   EventTarget.prototype.off = EventTarget.prototype.removeEventListener
   const DEBUG   = location.hash === '#debug'
-  const VERSION = 'v0.2.1'
+  const VERSION = 'v0.2.2'
   const log = DEBUG ? console.log.bind(console) : () => {}
   log('sw.js')
 
