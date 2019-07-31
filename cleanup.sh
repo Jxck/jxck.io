ee92ee3bd10cc08c26e5c748f844adb3eb1e1b81
Jxck <block.rxckin.beats@gmail.com>
2019-07-31 13:25:40 +0900

    [sh] remove cleanup.sh

--- cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for i in `seq 1 ${#}`
-do
-  echo
-  echo "rm ${1}"
-  selects path from "./${1}/**/*" where extname '=~' 'gz|br' | q | tee -a /dev/stderr | xargs -L 1 -P 4 rm
-  shift
-done
