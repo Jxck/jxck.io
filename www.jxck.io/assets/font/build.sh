6cde0835e6f089f959a32af05531168134688b07
Jxck <block.rxckin.beats@gmail.com>
2019-10-09 23:22:09 +0900

    [font] rename deploy.sh to build.sh

---
+++ www.jxck.io/assets/font/build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/zsh
+
+NOW=$(date +"%Y%m%d")
+
+\rm -rf ./tmp
+\mkdir ./tmp
+cd ./tmp
+wget https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip
+unzip NotoSansCJKjp-hinted.zip
+
+
+TARGETS=(
+"NotoSansCJKjp-Bold.otf"
+"NotoSansCJKjp-Regular.otf"
+"NotoSansMonoCJKjp-Bold.otf"
+"NotoSansMonoCJKjp-Regular.otf"
+)
+
+foreach target in $TARGETS
+ woff=`echo $target | sed "s/.otf/-Jxck-$NOW.woff/g"`
+ woff2=`echo $target | sed "s/.otf/-Jxck-$NOW.woff2/g"`
+ pyftsubset $target --text-file=../All.txt --layout-features='*' --flavor=woff2 --output-file=$woff2
+ mv $woff2 ../
+end
+
+sed -i -e "s/2019[0-9]\{2\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/.mruby.handler/*
+sed -i -e "s/2019[0-9]\{2\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/.template/*
+sed -i -e "s/2019[0-9]\{2\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/labs.jxck.io/**/*.html
+sed -i -e "s/2019[0-9]\{2\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/www.jxck.io/assets/js/*.js
+sed -i -e "s/2019[0-9]\{2\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/www.jxck.io/assets/css/*.css
+
+\rm -rf ./tmp
