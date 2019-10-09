422af195a19635a15d6fbad653725510e7dfd82f
Jxck <block.rxckin.beats@gmail.com>
2019-10-09 23:09:01 +0900

    [font] fixup font subsetting

--- www.jxck.io/assets/font/deploy.sh
+++ www.jxck.io/assets/font/deploy.sh
@@ -1,21 +1,46 @@
-mv NotoSansCJKjp-Bold-Jxck-201908.otf             NotoSansCJKjp-Bold-Jxck-201909.otf
-mv NotoSansCJKjp-Bold-Jxck-201908.woff            NotoSansCJKjp-Bold-Jxck-201909.woff
-mv NotoSansCJKjp-Bold-Jxck-201908.woff2           NotoSansCJKjp-Bold-Jxck-201909.woff2
-mv NotoSansCJKjp-Regular-Jxck-201908.otf          NotoSansCJKjp-Regular-Jxck-201909.otf
-mv NotoSansCJKjp-Regular-Jxck-201908.woff         NotoSansCJKjp-Regular-Jxck-201909.woff
-mv NotoSansCJKjp-Regular-Jxck-201908.woff2        NotoSansCJKjp-Regular-Jxck-201909.woff2
-mv NotoSansMonoCJKjp-Bold-Jxck-201908.otf         NotoSansMonoCJKjp-Bold-Jxck-201909.otf
-mv NotoSansMonoCJKjp-Bold-Jxck-201908.woff        NotoSansMonoCJKjp-Bold-Jxck-201909.woff
-mv NotoSansMonoCJKjp-Bold-Jxck-201908.woff2       NotoSansMonoCJKjp-Bold-Jxck-201909.woff2
-mv NotoSansMonoCJKjp-Regular-Jxck-201908.otf      NotoSansMonoCJKjp-Regular-Jxck-201909.otf
-mv NotoSansMonoCJKjp-Regular-Jxck-201908.woff     NotoSansMonoCJKjp-Regular-Jxck-201909.woff
-mv NotoSansMonoCJKjp-Regular-Jxck-201908.woff2    NotoSansMonoCJKjp-Regular-Jxck-201909.woff2
+#!/usr/bin/zsh
 
-sed -i -e "s/201908.woff2/201909.woff2/g" ~/server/jxck.io/.mruby.handler/*
-sed -i -e "s/201908.woff2/201909.woff2/g" ~/server/jxck.io/.template/*
-sed -i -e "s/201908.woff2/201909.woff2/g" ~/server/jxck.io/labs.jxck.io/**/*.html
-sed -i -e "s/201908.woff2/201909.woff2/g" ~/server/jxck.io/www.jxck.io/assets/js/*.js
-sed -i -e "s/201908.woff2/201909.woff2/g" ~/server/jxck.io/www.jxck.io/assets/css/*.css
+\rm -rf ./tmp
+\mkdir ./tmp
+cd ./tmp
+wget https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip
+unzip NotoSansCJKjp-hinted.zip
 
-cd ~/server/jxck.io
-make full
+
+NOW=$(date +"%Y%m%d")
+
+TARGETS=(
+"NotoSansCJKjp-Bold.otf"
+"NotoSansCJKjp-Regular.otf"
+"NotoSansMonoCJKjp-Bold.otf"
+"NotoSansMonoCJKjp-Regular.otf"
+)
+
+foreach target in $TARGETS
+ woff=`echo $target | sed "s/.otf/-Jxck-$NOW.woff/g"`
+ woff2=`echo $target | sed "s/.otf/-Jxck-$NOW.woff2/g"`
+ pyftsubset $target --text-file=../All.txt --layout-features='*' --flavor=woff  --output-file=$woff
+ pyftsubset $target --text-file=../All.txt --layout-features='*' --flavor=woff2 --output-file=$woff2
+end
+
+#mv NotoSansCJKjp-Bold-Jxck-201908.otf             NotoSansCJKjp-Bold-Jxck-201909.otf
+#mv NotoSansCJKjp-Bold-Jxck-201908.woff            NotoSansCJKjp-Bold-Jxck-201909.woff
+#mv NotoSansCJKjp-Bold-Jxck-201908.woff2           NotoSansCJKjp-Bold-Jxck-201909.woff2
+#mv NotoSansCJKjp-Regular-Jxck-201908.otf          NotoSansCJKjp-Regular-Jxck-201909.otf
+#mv NotoSansCJKjp-Regular-Jxck-201908.woff         NotoSansCJKjp-Regular-Jxck-201909.woff
+#mv NotoSansCJKjp-Regular-Jxck-201908.woff2        NotoSansCJKjp-Regular-Jxck-201909.woff2
+#mv NotoSansMonoCJKjp-Bold-Jxck-201908.otf         NotoSansMonoCJKjp-Bold-Jxck-201909.otf
+#mv NotoSansMonoCJKjp-Bold-Jxck-201908.woff        NotoSansMonoCJKjp-Bold-Jxck-201909.woff
+#mv NotoSansMonoCJKjp-Bold-Jxck-201908.woff2       NotoSansMonoCJKjp-Bold-Jxck-201909.woff2
+#mv NotoSansMonoCJKjp-Regular-Jxck-201908.otf      NotoSansMonoCJKjp-Regular-Jxck-201909.otf
+#mv NotoSansMonoCJKjp-Regular-Jxck-201908.woff     NotoSansMonoCJKjp-Regular-Jxck-201909.woff
+#mv NotoSansMonoCJKjp-Regular-Jxck-201908.woff2    NotoSansMonoCJKjp-Regular-Jxck-201909.woff2
+#
+#sed -i -e "s/201908.woff2/201909.woff2/g" ~/server/jxck.io/.mruby.handler/*
+#sed -i -e "s/201908.woff2/201909.woff2/g" ~/server/jxck.io/.template/*
+#sed -i -e "s/201908.woff2/201909.woff2/g" ~/server/jxck.io/labs.jxck.io/**/*.html
+#sed -i -e "s/201908.woff2/201909.woff2/g" ~/server/jxck.io/www.jxck.io/assets/js/*.js
+#sed -i -e "s/201908.woff2/201909.woff2/g" ~/server/jxck.io/www.jxck.io/assets/css/*.css
+#
+#cd ~/server/jxck.io
+#make full
--- www.jxck.io/assets/font/font.rb
+++ www.jxck.io/assets/font/font.rb
@@ -1,48 +1,26 @@
 #!/usr/bin/env ruby
-# このスクリプトを実行すると、現時点のエントリの中から全ての文字を取り出す
-# そこから Ignore.txt の文字を省く
-# それと All.txt の差分を表示
-#
-# この出力を All.txt に入れてフォントセットを作り直す。
-# そこから https://noto-website.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip をベースに
-# http://opentype.jp/subsetfontmk.htm を使って生成し
-# http://opentype.jp/woffconv.htm で woff にする
-# バージョンをつけてデプロイする。
-current = Dir.glob("../../../blog.jxck.io/entries/**/*.md").map {|file|
-  File.read(file).gsub("\n", "").gsub(/(.)/, '\\1\n')
-}.join("").split('\n').sort.uniq
+
+NOTO = "../../../blog.jxck.io/entries/2016-03-14/web-font-noto-sans.html"
+UNI  = "../../../blog.jxck.io/entries/2017-03-02/unicode-in-javascript.html"
+ALL  = "../../../blog.jxck.io/entries/**/*.html"
+
+current = Dir.glob(ALL).reject {|file|
+  file.include?(".amp.html")
+}.map {|file|
+  File.read(file)
+    .gsub(/<code.*?<\/code>/m, '')
+    .gsub("\n", "")
+    .gsub(/(.)/, '\\1\n')
+}.join("").split('\n').sort.uniq.tap{|chars|
+  chars.each{|char|
+    # puts "#{char}: #{char.codepoints.map{|c| '0x'+c.to_s(16)}}"
+  }
+}
 
 all = File.read("./All.txt").split("\n")
 
-# これらはフォントのサブセットに含めない
-ignore = [
-  "",       # empty
-  "\u0009", # tab
-  "\u0020", # space
+current = current - [" "]
 
-  # unicode-in-javascript の中で出てくるやつ
-  # "\u0304", # combining macron
-  # "\u0308", # combining diaeresis
-  # "\u200d", # zero width joiner
-  # "\u309a", # 片仮名の丸
-  # "\ufffd", # replacement character
-  # "🏻",
-  # "🏼",
-  # "🏽",
-  # "🏾",
-  # "🏿",
-  # "👍",
-  # "👦",
-  # "👧",
-  # "👨",
-  # "👩",
-  # "😭",
-  # ["E0100".hex].pack('U*'), # variation selector 17
-]
+p (current - all)
 
-
-diff = (current - all - ignore)
-
-diff.each{|char|
-  puts "#{char}: #{char.codepoints.map{|c| '0x'+c.to_s(16)}}"
-}
+IO.write("./All.txt", current.join("\n"))
