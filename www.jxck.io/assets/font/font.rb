70ec9d2870835ec0705f284fcdf9b5497aa04edb
Jxck <block.rxckin.beats@gmail.com>
2019-10-09 15:45:58 +0900

    [font] update font.rb

--- www.jxck.io/assets/font/font.rb
+++ www.jxck.io/assets/font/font.rb
@@ -5,16 +5,45 @@
 # http://opentype.jp/subsetfontmk.htm を使って生成し
 # http://opentype.jp/woffconv.htm で woff にする
 # バージョンをつけてデプロイする。
-current = Dir.glob("../../../blog.jxck.io/entries/**/*.md").select {|file|
-  file != "../../../blog.jxck.io/entries/2016-03-14/web-font-noto-sans.md"
+current = Dir.glob("../../../blog.jxck.io/entries/**/*.md").reject {|file|
+  [
+    "../../../blog.jxck.io/entries/2016-03-14/web-font-noto-sans.md",
+    "../../../blog.jxck.io/entries/2017-03-02/unicode-in-javascript.md",
+  ].include?(file)
 }.map {|file|
   File.read(file).gsub("\n", "").gsub(/(.)/, '\\1\n')
 }.join("").split('\n').sort.uniq
 
 all = File.read("./All.txt").split("\n")
 
-diff = (current - all - ["", " ", "\t"])
+# これらはフォントのサブセットに含めない
+ignore = [
+  "",       # empty
+  "\u0009", # tab
+  "\u0020", # space
+
+  # unicode-in-javascript の中で出てくるやつ
+  # "\u0304", # combining macron
+  # "\u0308", # combining diaeresis
+  # "\u200d", # zero width joiner
+  # "\u309a", # 片仮名の丸
+  # "\ufffd", # replacement character
+  # "🏻",
+  # "🏼",
+  # "🏽",
+  # "🏾",
+  # "🏿",
+  # "👍",
+  # "👦",
+  # "👧",
+  # "👨",
+  # "👩",
+  # "😭",
+  # ["E0100".hex].pack('U*'), # variation selector 17
+]
+
+diff = (current - all - ignore)
 
 diff.each{|char|
-  puts "#{char}: #{char.codepoints}"
+  puts "#{char}: #{char.codepoints.map{|c| '0x'+c.to_s(16)}}"
 }
