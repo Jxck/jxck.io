d4626b8c2fe1a2aadb91e7d081f0647d2ed86c05
Jxck <block.rxckin.beats@gmail.com>
2019-10-11 13:37:09 +0900

    [font] fixup regex build.sh

--- www.jxck.io/assets/font/build.sh
+++ www.jxck.io/assets/font/build.sh
@@ -23,11 +23,11 @@ foreach target in $TARGETS
  mv $woff2 ../
 end
 
-sed -i -e "s/2019[0-9]\{2\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/.mruby.handler/*
-sed -i -e "s/2019[0-9]\{2\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/.template/*
-sed -i -e "s/2019[0-9]\{2\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/labs.jxck.io/**/*.html
-sed -i -e "s/2019[0-9]\{2\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/www.jxck.io/assets/js/*.js
-sed -i -e "s/2019[0-9]\{2\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/www.jxck.io/assets/css/*.css
+sed -i -e "s/2019[0-9]\{4\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/.mruby.handler/*
+sed -i -e "s/2019[0-9]\{4\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/.template/*
+sed -i -e "s/2019[0-9]\{4\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/labs.jxck.io/**/*.html
+sed -i -e "s/2019[0-9]\{4\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/www.jxck.io/assets/js/*.js
+sed -i -e "s/2019[0-9]\{4\}\.woff2/$NOW.woff2/g" ~/server/jxck.io/www.jxck.io/assets/css/*.css
 
 cd ../
 \rm -rf ./tmp
