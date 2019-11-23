d15d42dd446ce57e007fc2b09d917d37f87af75d
Jxck <block.rxckin.beats@gmail.com>
2019-11-24 01:32:52 +0900

    [assets] up main.js

--- www.jxck.io/assets/js/main.js
+++ www.jxck.io/assets/js/main.js
@@ -31,6 +31,68 @@ if (window.ReportingObserver) {
       navigator.sendBeacon(URL, JSON.stringify(r))
     }
   }, {buffered: true})
-
   observer.observe()
+}
+
+
+if (window.PerformanceObserver) {
+  const performance = {}
+
+  const observer = new PerformanceObserver((list) => {
+    for (const entry of list.getEntries()) {
+      switch(entry.entryType) {
+        case "navigation":
+          const connect  = entry.connectEnd                           // TCP establish
+          const request  = entry.responseStart - entry.requestStart   // request time
+          const response = entry.responseEnd   - entry.responseStart  // response time
+          const dom      = entry.domComplete   - entry.responseEnd    // dom time
+          const load     = entry.loadEventEnd  - entry.loadEventStart // load
+          const duration = entry.duration                             // duration
+          performance.navigation = {
+            "0connect":  connect,
+            "1request":  request,
+            "2response": response,
+            "3dom":      dom,
+            "4load":     load,
+            "5duration": duration,
+          }
+          break;
+        case "largest-contentful-paint":
+          performance.lcp = performance.lcp || []
+          const lcp = {
+            elem: entry.element.nodeName,
+            time: entry.renderTime || entry.loadTime
+          }
+          performance.lcp.push(lcp);
+          break;
+        case "paint":
+          performance[entry.name] = entry.startTime
+          break;
+        case "resource":
+          performance.resource = performance.resource || {}
+          performance.resource[entry.name] = entry.duration
+          break;
+        case "layout-shift":
+          performance.layoutshift = performance.layoutshift || 0
+          performance.layoutshift += entry.value
+          break;
+        case "longtask":
+          performance.longtask += entry.duration
+          break;
+        case "first-input":
+          console.log(entry)
+          break;
+        default:
+          console.log(entry)
+      }
+    }
+  })
+  observer.observe({entryTypes: PerformanceObserver.supportedEntryTypes});
+
+  setTimeout(() => {
+    observer.takeRecords()
+    observer.disconnect()
+    // console.log(JSON.stringify(performance, " ", " "))
+    console.log(performance)
+  }, 3000)
 }
