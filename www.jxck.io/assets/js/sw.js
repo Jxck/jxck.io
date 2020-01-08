8a4e996f975a36f2149c2cb5171db561cb37dec8
Jxck <block.rxckin.beats@gmail.com>
2020-01-08 22:02:03 +0900

    build

--- www.jxck.io/assets/js/sw.js
+++ www.jxck.io/assets/js/sw.js
@@ -39,10 +39,10 @@
       log('worker()', self)
 
       const ASSETS = [
-        'https://jxck.io/assets/font/NotoSansCJKjp-Regular-Jxck-20191017.woff2',
-        'https://jxck.io/assets/font/NotoSansCJKjp-Bold-Jxck-20191017.woff2',
-        'https://jxck.io/assets/font/NotoSansMonoCJKjp-Regular-Jxck-20191017.woff2',
-        'https://jxck.io/assets/font/NotoSansMonoCJKjp-Bold-Jxck-20191017.woff2',
+        'https://jxck.io/assets/font/NotoSansCJKjp-Regular-Jxck-20200109.woff2',
+        'https://jxck.io/assets/font/NotoSansCJKjp-Bold-Jxck-20200109.woff2',
+        'https://jxck.io/assets/font/NotoSansMonoCJKjp-Regular-Jxck-20200109.woff2',
+        'https://jxck.io/assets/font/NotoSansMonoCJKjp-Bold-Jxck-20200109.woff2',
         'https://jxck.io/assets/js/highlight.pack.js',
       ]
 
