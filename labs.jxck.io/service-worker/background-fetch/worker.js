e1fb83492d6838833ac6a4d2563d51ac519472b8
Jxck <block.rxckin.beats@gmail.com>
2020-01-22 22:04:03 +0900

    [labs] update background fetch

--- labs.jxck.io/service-worker/background-fetch/worker.js
+++ labs.jxck.io/service-worker/background-fetch/worker.js
@@ -1,5 +1,7 @@
 console.info('worker')
 
+const CACHE_NAME = "v0.0.0"
+
 self.addEventListener('install', (e) => {
   console.info('install', e)
   e.waitUntil(skipWaiting())
@@ -13,49 +15,38 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('backgroundfetchsuccess', (e) => {
   console.log(e.type)
   e.waitUntil(async function() {
-    // 保存
     try {
-      console.log(e.registration.id)
+      // 結果を取り出す
+      const id = e.registration.id
+      const record = await e.registration.match(id)
+      console.log(id, record)
 
-      const cache = await caches.open(e.registration.id)
-      console.log(cache)
+      // キャッシュ対象
+      const request = record.request
+      const response = await record.responseReady
+      console.log(request, response)
 
-      const records = await e.registration.matchAll()
-      console.log(records)
+      // キャッシュ先
+      const cache = await caches.open(CACHE_NAME)
+      await cache.put(request.url, response)
 
-      const promises = records.map(async (record) => {
-        const response = await record.responseReady
-        console.log(response)
-        await cache.put(record.request, response)
-      })
-
-      await Promise.all(promises)
-      await e.updateUI({ title: 'downloaded!' })
+      // 通知
+      await e.updateUI({ title: id })
     } catch (err) {
-      e.updateUI({ title: `Movie download failed` })
+      console.error(err)
+      e.updateUI({ title: `download failed ${e.registration.id}` })
     }
   }())
 })
 
-// 失敗した場合
 self.addEventListener('backgroundfetchfail', (e) => {
-  console.log(e)
-  e.waitUntil(async function() {
-    // 成功した分だけ保存
-    try {
-      const cache    = await caches.open(e.registration.id)
-      const records  = await e.registration.matchAll()
-      const promises = records.map(async (record) => {
-        const response = await record.responseReady.catch(() => undefined)
-        if (response && response.ok) {
-          await cache.put(record.request, response)
-        }
-      })
-      await Promise.all(promises)
-    } finally {
-      await e.updateUI({ title: `download failed.` })
-    }
-  }())
+  // 特に何もする必要ないっぽい
+  console.error(e)
+})
+
+self.addEventListener('backgroundfetchabort', (e) => {
+  // 特に何もする必要ないっぽい
+  console.error(e)
 })
 
 // download タスクをクリックした場合
@@ -64,6 +55,7 @@ self.addEventListener('backgroundfetchclick', (e) => {
   e.waitUntil(async function() {
     const url = e.registration.id
     console.log(url)
+    // TODO: エピソードのページを開く
     clients.openWindow(url)
   }())
 })
@@ -71,12 +63,14 @@ self.addEventListener('backgroundfetchclick', (e) => {
 // ダウンロードしたものを返す
 self.addEventListener('fetch', (e) => {
   const url = e.request.url
-  if (!url.endsWith('.zip')) return
   console.log(url)
 
   e.respondWith(async function() {
-    const cachedResponse = await caches.match(e.request)
-    if (cachedResponse) return cachedResponse
+    const cachedResponse = await caches.match(e.request.url)
+    if (cachedResponse) {
+      console.log("hit", cachedResponse)
+      return cachedResponse
+    }
 
     // Maybe it's mid-download?
     const bgFetch = await registration.backgroundFetch.get(url)
