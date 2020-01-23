c6598ee008ddaa0158cc3812ebeaa8ca64def332
Jxck <block.rxckin.beats@gmail.com>
2020-01-23 19:35:32 +0900

    [labs] up periodic

--- labs.jxck.io/service-worker/periodic-background-sync/worker.js
+++ labs.jxck.io/service-worker/periodic-background-sync/worker.js
@@ -13,11 +13,7 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('periodicsync', (e) => {
   console.log('periodicsync', e)
   e.waitUntil(async function() {
-    const res   = await fetch('./feed.txt')
-    const date  = res.headers.get('date')
-    const url   = new URL(res.url)
-    url.search  = date
     const cache = await caches.open('periodic-background-sync')
-    return cache.put(url, res)
+    return cache.add(`./?q=${Date.now()}`, res)
   }())
 })
