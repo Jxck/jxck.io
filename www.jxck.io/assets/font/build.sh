67c3386eed62ed8218aaaf52c3a2a33ef6625fba
Jxck <block.rxckin.beats@gmail.com>
2020-02-11 19:26:38 +0900

    [font] uncommend build.sh

--- www.jxck.io/assets/font/build.sh
+++ www.jxck.io/assets/font/build.sh
@@ -2,28 +2,28 @@
 
 NOW=$(date +"%Y%m%d")
 
-#\rm -rf ./tmp
-#\mkdir ./tmp
-#cd ./tmp
-#wget https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip
-#unzip NotoSansCJKjp-hinted.zip
-#
-#
-#TARGETS=(
-#"NotoSansCJKjp-Bold.otf"
-#"NotoSansCJKjp-Regular.otf"
-#"NotoSansMonoCJKjp-Bold.otf"
-#"NotoSansMonoCJKjp-Regular.otf"
-#)
-#
-#FEATURES='palt'
-#
-#foreach target in $TARGETS
-# woff=`echo $target | sed "s/.otf/-Jxck-$NOW.woff/g"`
-# woff2=`echo $target | sed "s/.otf/-Jxck-$NOW.woff2/g"`
-# pyftsubset $target --text-file=../All.txt --layout-features=$FEATURES --flavor=woff2 --output-file=$woff2
-# mv $woff2 ../
-#end
+\rm -rf ./tmp
+\mkdir ./tmp
+cd ./tmp
+wget https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip
+unzip NotoSansCJKjp-hinted.zip
+
+
+TARGETS=(
+"NotoSansCJKjp-Bold.otf"
+"NotoSansCJKjp-Regular.otf"
+"NotoSansMonoCJKjp-Bold.otf"
+"NotoSansMonoCJKjp-Regular.otf"
+)
+
+FEATURES='palt'
+
+foreach target in $TARGETS
+ woff=`echo $target | sed "s/.otf/-Jxck-$NOW.woff/g"`
+ woff2=`echo $target | sed "s/.otf/-Jxck-$NOW.woff2/g"`
+ pyftsubset $target --text-file=../All.txt --layout-features=$FEATURES --flavor=woff2 --output-file=$woff2
+ mv $woff2 ../
+end
 
 sed -i -e "s/2020[0-9]\{4\}\.woff2/$NOW.woff2/g" $SERVER/jxck.io/.mruby.handler/*
 sed -i -e "s/2020[0-9]\{4\}\.woff2/$NOW.woff2/g" $SERVER/jxck.io/.script/template/*.erb
@@ -31,5 +31,5 @@ sed -i -e "s/2020[0-9]\{4\}\.woff2/$NOW.woff2/g" $SERVER/jxck.io/labs.jxck.io/**
 sed -i -e "s/2020[0-9]\{4\}\.woff2/$NOW.woff2/g" $SERVER/jxck.io/www.jxck.io/assets/js/*.js
 sed -i -e "s/2020[0-9]\{4\}\.woff2/$NOW.woff2/g" $SERVER/jxck.io/www.jxck.io/assets/css/*.css
 
-#cd ../
-#\rm -rf ./tmp
+cd ../
+\rm -rf ./tmp
