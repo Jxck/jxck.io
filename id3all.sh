95ed352db4f9b31f68393b98a0176001258ce42d
Jxck <block.rxckin.beats@gmail.com>
2020-02-11 18:42:40 +0900

    build

--- id3all.sh
+++ id3all.sh
@@ -898,3 +898,16 @@ eyeD3 --title "ep62 Monthly Web 202001" \
 
 
 
+eyeD3 --remove-all ../files.mozaic.fm/mozaic-ep63.mp3
+
+eyeD3 --title "ep63 Cross Origin Info Leaks" \
+      --track 70 \
+      --artist 'Jxck' \
+      --album 'mozaic.fm' \
+      --genre 'Podcast' \
+      --add-image ./www.jxck.io/assets/img/mozaic.jpeg:FRONT_COVER \
+      --to-v2.3 \
+      ../files.mozaic.fm/mozaic-ep63.mp3
+
+
+
