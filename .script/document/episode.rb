44ef459286d6a747f2d6b9ac0e42b31c85485a95
Jxck <block.rxckin.beats@gmail.com>
2020-02-14 00:51:01 +0900

    [template] add crossorigin to mozaic.fm audio

--- .script/document/episode.rb
+++ .script/document/episode.rb
@@ -42,7 +42,7 @@ module Document
 
     def article
       super
-        .sub(/audio: (.*)/, "<mozaic-player><audio slot=audio src=#{audio} title='#{title}' data-forward=+30 data-back=-10></audio></mozaic-player>")
+        .sub(/audio: (.*)/, "<mozaic-player><audio slot=audio src=#{audio} crossorigin=anonymous title='#{title}' data-forward=+30 data-back=-10></audio></mozaic-player>")
         .sub(/<dl>(.*?)<dt>published_at/m, '<dl class=info>\1<dt>published_at')
         .sub(/published_at: (.*)/, "published_at: <time datetime=#{datetime}>#{datetime}</time>")
     end
