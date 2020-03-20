e7dfea54a91c5582500d7e61efeb4651053e8019
Jxck <block.rxckin.beats@gmail.com>
2020-03-21 01:21:04 +0900

    [admin] remove cert-bootcamp.sh

--- admin.jxck.io/cert-bootcamp.sh
+++
@@ -1,9 +0,0 @@
-sudo certbot certonly \
-  -n \
-  --webroot \
-  --agree-tos \
-  --email admin@jxck.io \
-  -w ../www.jxck.io \
-  --expand \
-  -d bootcamp.labs.jxck.io \
-  -d bootcamp2.labs.jxck.io
