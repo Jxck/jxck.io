dea383da624c8c0b961bee93d13da3952f0c2685
Jxck <block.rxckin.beats@gmail.com>
2020-04-08 18:18:41 +0900

    [labs] up content-index api

--- labs.jxck.io/service-worker/content-index/worker.js
+++ labs.jxck.io/service-worker/content-index/worker.js
@@ -2,31 +2,7 @@ console.info('worker')
 
 self.addEventListener('install', async (e) => {
   console.info(e.type, e)
-
-  console.log('index', self.registration.index)
-
-  if ('index' in self.registration) {
-    await self.registration.index.add({
-      id: '01',
-      title: 'title for content',
-      description: 'description for content',
-      category: 'article',
-      icons: [
-        {
-          src: 'https://logo.jxck.io/jxck.60x60.png',
-          sizes: '60x60',
-          type: 'image/png',
-        },
-        {
-          src: 'https://logo.jxck.io/jxck.1200x1200.png',
-          sizes: '1200x1200',
-          type: 'image/png',
-        },
-      ],
-      launchUrl: 'https://blog.jxck.io'
-    })
-  }
-
+  console.log(self.registration.index)
   e.waitUntil(skipWaiting())
 })
 
