8acb5a7eef7685aafc96cef4417573eb0b8ae557
Jxck <block.rxckin.beats@gmail.com>
2020-04-15 07:31:23 +0900

    [labs] up ls

--- labs.jxck.io/rust/ls/src/main.rs
+++ labs.jxck.io/rust/ls/src/main.rs
@@ -1,7 +1,7 @@
 extern crate glob;
 extern crate rand;
 
-use glob::glob;
+use std::cmp;
 use std::fmt;
 use std::fs;
 use std::os::unix::fs::{FileTypeExt, MetadataExt};
@@ -12,56 +12,59 @@ type GroupMode = (bool, bool, bool);
 type OtherMode = (bool, bool, bool);
 type Mode = (UserMode, GroupMode, OtherMode);
 
+#[derive(PartialEq, PartialOrd, Eq, Ord)]
+enum FileType {
+    // TODO: 値いらない？
+    Dir = 1,
+    Pipe = 2,
+    Socket = 3,
+    Block = 4,
+    Char = 5,
+    Sym = 6,
+    Exec = 7,
+    File = 8,
+}
+
+#[allow(dead_code)]
 struct Node {
     entry: fs::DirEntry,
-    file_type: fs::FileType,
+    file_type: FileType,
+    mode: Mode,
 }
 impl Node {
     fn new(entry: fs::DirEntry) -> Node {
-        let file_type = entry.file_type().unwrap();
+        let (file_type, mode) = Node::file_type(&entry);
         let node = Node {
-            entry: entry,
-            file_type: file_type,
+            entry,
+            file_type,
+            mode,
         };
         return node;
     }
-    fn is_dir(&self) -> bool {
-        return self.file_type.is_dir();
+    fn file_type(entry: &fs::DirEntry) -> (FileType, Mode) {
+        let file_type = entry.file_type().unwrap();
+        let mode = Node::permission(&entry);
+        let file_type: FileType = if file_type.is_dir() {
+            FileType::Dir
+        } else if file_type.is_fifo() {
+            FileType::Pipe
+        } else if file_type.is_socket() {
+            FileType::Socket
+        } else if file_type.is_block_device() {
+            FileType::Block
+        } else if file_type.is_char_device() {
+            FileType::Char
+        } else if file_type.is_symlink() {
+            FileType::Sym
+        } else if Node::is_exec(mode) {
+            FileType::Exec
+        } else {
+            FileType::File
+        };
+        return (file_type, mode);
     }
-    #[allow(dead_code)]
-    fn is_file(&self) -> bool {
-        return self.file_type.is_file();
-    }
-    #[allow(dead_code)]
-    fn is_fifo(&self) -> bool {
-        return self.file_type.is_fifo();
-    }
-    fn is_socket(&self) -> bool {
-        return self.file_type.is_socket();
-    }
-    fn is_block_device(&self) -> bool {
-        return self.file_type.is_block_device();
-    }
-    fn is_char_device(&self) -> bool {
-        return self.file_type.is_char_device();
-    }
-    fn is_symlink(&self) -> bool {
-        return self.file_type.is_symlink();
-    }
-    fn path(&self) -> PathBuf {
-        return self.entry.path();
-    }
-    fn file_name(&self) -> String {
-        return self
-            .path()
-            .file_name()
-            .unwrap()
-            .to_str()
-            .unwrap()
-            .to_string(); // ??
-    }
-    fn permission(&self) -> Mode {
-        let mode: u32 = self.entry.metadata().unwrap().mode();
+    fn permission(entry: &fs::DirEntry) -> Mode {
+        let mode: u32 = entry.metadata().unwrap().mode();
         let user: UserMode = (
             mode & 0b100000000 > 0, // r
             mode & 0b010000000 > 0, // w
@@ -79,8 +82,7 @@ impl Node {
         );
         return (user, group, other);
     }
-    fn is_exec(&self) -> bool {
-        let mode: Mode = self.permission();
+    fn is_exec(mode: Mode) -> bool {
         return match mode {
             ((_, _, true), _, _) => true,
             (_, (_, _, true), _) => true,
@@ -88,76 +90,122 @@ impl Node {
             _ => false,
         };
     }
+    fn path(&self) -> PathBuf {
+        return self.entry.path();
+    }
+    fn file_name(&self) -> String {
+        return self
+            .path()
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string(); // ??
+    }
+    // fn is_dir(&self) -> bool {
+    //     return self.file_type.is_dir();
+    // }
+    // #[allow(dead_code)]
+    // fn is_file(&self) -> bool {
+    //     return self.file_type.is_file();
+    // }
+    // #[allow(dead_code)]
+    // fn is_fifo(&self) -> bool {
+    //     return self.file_type.is_fifo();
+    // }
+    // fn is_socket(&self) -> bool {
+    //     return self.file_type.is_socket();
+    // }
+    // fn is_block_device(&self) -> bool {
+    //     return self.file_type.is_block_device();
+    // }
+    // fn is_char_device(&self) -> bool {
+    //     return self.file_type.is_char_device();
+    // }
+    // fn is_symlink(&self) -> bool {
+    //     return self.file_type.is_symlink();
+    // }
+}
+
+impl Eq for Node {}
+
+impl cmp::Ord for Node {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        // sort by file_type
+        match self.file_type.cmp(&other.file_type) {
+            cmp::Ordering::Less => cmp::Ordering::Less,
+            cmp::Ordering::Greater => cmp::Ordering::Greater,
+            cmp::Ordering::Equal => {
+                // sort by file name in number or string
+                let self_name = self.file_name();
+                let other_name = &other.file_name();
+                return match self_name.parse::<u8>() {
+                    Ok(self_num) => match &other_name.parse::<u8>() {
+                        Ok(other_num) => self_num.cmp(other_num),
+                        Err(_) => self_name.cmp(other_name),
+                    },
+                    Err(_) => self_name.cmp(other_name),
+                };
+            }
+        }
+    }
+}
+
+impl cmp::PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.path() == other.path()
+    }
+}
+
+impl cmp::PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 impl fmt::Display for Node {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if self.is_dir() {
-            // dir -> navy
-            write!(f, "\x1b[0;34m{}\x1b[0m", self.file_name())
-        } else if self.is_symlink() {
-            let link = std::fs::read_link(self.path()).unwrap();
-            // sym -> cyan
-            write!(
-                f,
-                "\x1b[0;36m{} -> {}\x1b[0m",
-                self.file_name(),
-                link.display()
-            )
-        } else if self.is_exec() {
-            // exec -> red
-            write!(f, "\x1b[0;31m{}\x1b[0m", self.file_name())
-        } else if self.is_socket() {
-            // socket -> yellow
-            write!(f, "\x1b[1;33m{}\x1b[0m", self.file_name())
-        } else if self.is_block_device() {
-            // block -> pink
-            write!(f, "\x1b[1;35m{}\x1b[0m", self.file_name())
-        } else if self.is_char_device() {
-            // char -> aqua
-            write!(f, "\x1b[1;36m{}\x1b[0m", self.file_name())
-        } else if self.is_fifo() {
-            // pipe -> orange
-            write!(f, "\x1b[1;31m{}\x1b[0m", self.file_name())
-        } else {
-            write!(f, "{}", self.path().file_name().unwrap().to_str().unwrap())
+        match self.file_type {
+            FileType::Dir => write!(f, "\x1b[0;34m{}\x1b[0m", self.file_name()), // dir -> navy
+            FileType::Pipe => write!(f, "\x1b[1;31m{}\x1b[0m", self.file_name()), // pipe -> orange
+            FileType::Socket => write!(f, "\x1b[1;33m{}\x1b[0m", self.file_name()), // socket -> yellow
+            FileType::Block => write!(f, "\x1b[1;35m{}\x1b[0m", self.file_name()),  // block -> pink
+            FileType::Char => write!(f, "\x1b[1;36m{}\x1b[0m", self.file_name()),   // char -> aqua
+            FileType::Sym => {
+                // sym -> cyan
+                let link = std::fs::read_link(self.path()).unwrap();
+                write!(
+                    f,
+                    "\x1b[0;36m{}\x1b[0m -> {}",
+                    self.file_name(),
+                    link.display()
+                )
+            }
+            FileType::Exec => write!(f, "\x1b[0;31m{}\x1b[0m", self.file_name()), // exec -> red
+            _ => write!(f, "{}", self.path().file_name().unwrap().to_str().unwrap()), // file
         }
     }
 }
 
 #[allow(dead_code)]
-fn mapped() {
-    let dir_entries: Vec<fs::DirEntry> = fs::read_dir(".").unwrap().map(|d| d.unwrap()).collect();
-    for entry in dir_entries {
-        let node: Node = Node::new(entry);
+fn list(path: &str) {
+    let mut nodes: Vec<Node> = fs::read_dir(path)
+        .unwrap()
+        .map(|d| Node::new(d.unwrap()))
+        .collect();
+
+    nodes.sort_unstable();
+    for node in nodes {
         println!("{}", node);
     }
 }
 
-#[allow(dead_code)]
-fn sorted() {
-    let mut entries: Vec<std::path::PathBuf> = fs::read_dir(".")
-        .unwrap()
-        .map(|entry| entry.unwrap().path())
-        .collect();
-    entries.sort_unstable();
-    for entry in entries {
-        println!("{:?}", entry);
-    }
-}
-#[allow(dead_code)]
-fn files() {
-    for entry in glob("./*").expect("Failed to read glob pattern") {
-        let path = entry.unwrap();
-        println!("{}", path.display());
-        let ext: &str = match path.extension() {
-            None => "",
-            Some(ext) => ext.to_str().unwrap(),
-        };
-        println!("{:?}", ext);
-    }
-}
-
+use std::env;
 fn main() {
-    mapped();
+    let args: Vec<String> = env::args().collect();
+    let mut path = ".";
+    if args.len() > 1 {
+        path = &args[1];
+    }
+    list(path);
 }
