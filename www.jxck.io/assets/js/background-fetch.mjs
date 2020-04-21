04115e41540680c59861b774b504793a02c42d0a
Jxck <block.rxckin.beats@gmail.com>
2020-04-22 01:44:30 +0900

    [mozaic] wrap bgfetch with <button>

--- www.jxck.io/assets/js/background-fetch.mjs
+++ www.jxck.io/assets/js/background-fetch.mjs
@@ -11,22 +11,33 @@ export default class BackgroundFetch extends HTMLElement {
           --ratio: 0;
           stroke-dasharray: calc(314*var(--ratio)) 314;
         }
+        button {
+          padding: 0;
+          margin: 0;
+          border: none;
+          object-fit: fill;
+          background-color: unset;
+          width: 100%;
+          height: 100%;
+        }
         </style>
 
-        <svg viewBox="0 0 100 100">
-          <circle id="base"     part="base"     cx="50" cy="50" r="46" stroke-width="8" transform="rotate(-90, 50, 50)" />
-          <circle id="progress" part="progress" cx="50" cy="50" r="46" stroke-width="8" transform="rotate(-90, 50, 50)" />
-          <path id="arrow" part="arrow" d="
-          M 40 20
-          L 60 20
-          L 60 50
-          L 75 50
-          L 50 80
-          L 25 50
-          L 40 50
-          L 40 20
-          "/>
-        </svg>
+        <button aria-label="download this audio" title="download this audio">
+          <svg viewBox="0 0 100 100">
+            <circle id="base"     part="base"     cx="50" cy="50" r="46" stroke-width="8" transform="rotate(-90, 50, 50)" />
+            <circle id="progress" part="progress" cx="50" cy="50" r="46" stroke-width="8" transform="rotate(-90, 50, 50)" />
+            <path id="arrow" part="arrow" d="
+            M 40 20
+            L 60 20
+            L 60 50
+            L 75 50
+            L 50 80
+            L 25 50
+            L 40 50
+            L 40 20
+            "/>
+          </svg>
+        </button>
       `
     return template.content.cloneNode(true)
   }
