bed05e8544731daa4a2c394c54a2013675c9e4a5
Jxck <block.rxckin.beats@gmail.com>
2020-06-01 16:48:32 +0900

    [labd] up webtrasnport

--- labs.jxck.io/webtransport/quictransport/start.sh
+++ labs.jxck.io/webtransport/quictransport/start.sh
@@ -1,1 +1,5 @@
-./server/quic_transport_simple_server --key_file=$DOTFILES/keys/privkey.p8 --certificate_file=$DOTFILES/keys/fullchain.pem --port=3000 --accepted_origins=""
+./server/quic_transport_simple_server  \
+  --key_file=$DOTFILES/keys/privkey.p8  \
+  --certificate_file=$DOTFILES/keys/fullchain.pem  \
+  --port=3000  \
+  --accepted_origins=""
