459ccae6c0e76bc3b65f50b4cd2c13ab5eaa6436
Jxck <block.rxckin.beats@gmail.com>
2020-06-24 16:12:01 +0900

    [labs] fixup server path for qt

--- labs.jxck.io/webtransport/quictransport/start.sh
+++ labs.jxck.io/webtransport/quictransport/start.sh
@@ -1,4 +1,4 @@
-/home/jxck/server/jxck.io/labs.jxck.io/webtransport/quictransport/server/quic_transport_simple_server  \
+$SERVER/jxck.io/labs.jxck.io/webtransport/quictransport/server/quic_transport_simple_server  \
   --key_file=/keys/privkey.p8  \
   --certificate_file=/keys/fullchain.pem  \
   --port=3000  \
