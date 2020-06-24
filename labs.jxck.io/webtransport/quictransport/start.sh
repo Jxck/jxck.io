b694ebecf9c769fe9fc19ee45be2b9c0edae1a75
Jxck <block.rxckin.beats@gmail.com>
2020-06-24 22:09:46 +0900

    [labs] replace quic-transport port to 7000

--- labs.jxck.io/webtransport/quictransport/start.sh
+++ labs.jxck.io/webtransport/quictransport/start.sh
@@ -1,5 +1,5 @@
 sudo ./server/quic_transport_simple_server  \
   --key_file=/keys/privkey.p8  \
   --certificate_file=/keys/fullchain.pem  \
-  --port=3000  \
+  --port=7000  \
   --accepted_origins=""
