feecde447c7d956db1724fa9ec5c2e42f219548c
Jxck <block.rxckin.beats@gmail.com>
2020-09-11 16:34:09 +0900

    [labs] add c/make

---
+++ labs.jxck.io/c/src/hello.c
@@ -0,0 +1,8 @@
+/* hello.c */
+#include <stdio.h>
+#include "print.h"
+
+int main(int argc, char *argv[]) {
+  print();
+  return 0;
+}
---
+++ labs.jxck.io/c/src/print.c
@@ -0,0 +1,7 @@
+/* print.c */
+#include <stdio.h>
+
+void print(void) {
+  printf("This is a test1.\n");
+  printf("This is a test2.\n");
+}
