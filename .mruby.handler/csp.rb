7b481a34d29688626bb4f4114a434fc9a88e2656
Jxck <block.rxckin.beats@gmail.com>
2020-09-14 00:50:28 +0900

    [mruby] fixup csp.rb

--- .mruby.handler/csp.rb
+++ .mruby.handler/csp.rb
@@ -5,38 +5,8 @@ Proc.new do |env|
     next [399, {}, []]
   end
 
-  headers = {}
-  if /\.amp.html\z/.match(env["PATH_INFO"])
-    # CSP for AMP Page
-    csp = [
-      "default-src",
-      "'self'",
-      "https://jxck.io",
-      "https://*.jxck.io",
-      "https://www.google-analytics.com",
-      "https://*.ampproject.org",
-      "https://*.ampproject.net",
-      "https://html.spec.whatwg.org",
-      "https://w3c.github.io",
-      "https://adservice.google.com",
-      ";",
-      "frame-src",
-      "https://www.youtube.com",
-      "https://*.doubleclick.net",
-      "https://*.googlesyndication.com",
-      ";",
-      "style-src",
-      "'unsafe-inline'",
-      ";",
-      "report-uri #{REPORT_URI}",
-      ";",
-      "report-to default"
-    ]
-    headers["Content-Security-Policy-Report-Only"] = csp.join(" ")
-  else
-    # CSP for Normal Page
-    csp = [
-      "default-src",
+  csp = {
+    "default-src": [
       "'self'",
       "https://jxck.io",
       "https://*.jxck.io",
@@ -50,38 +20,61 @@ Proc.new do |env|
       "https://*.googletagservices.com",
       "https://*.ytimg.com",
       "'unsafe-inline'",
-      ";",
-      "frame-src",
+    ],
+    "frame-src": [
       "https://www.youtube.com",
       "https://*.doubleclick.net",
       "https://*.googlesyndication.com",
-      ";",
-      "connect-src",
+    ],
+    "connect-src": [
       "wss://ws.jxck.io",
       "https://reporting.jxck.io",
       "https://*.gstatic.com",
       "https://www.google-analytics.com",
       "https://*.googlesyndication.com",
-      ";",
-      "report-uri #{REPORT_URI}",
-      ";",
-      "report-to default"
+      "https://*.doubleclick.net",
+    ],
+    "report-uri": [
+      "#{REPORT_URI}",
+    ],
+    "report-to": [
+      "default"
     ]
+  }
+  # CSP for Normal Page
+  # csp = [
+  #   "script-src",
+  #   "'strict-dynamic'",
+  #   "'unsafe-eval'",
+  #   "'unsafe-inline'",
+  #   "https:",
+  #   "'nonce-dummynonce'",
+  #   ";",
+  #   "report-uri #{REPORT_URI}",
+  #   ";",
+  #   "report-to default"
+  # ]
 
-    # csp = [
-    #   "script-src",
-    #   "'strict-dynamic'",
-    #   "'unsafe-eval'",
-    #   "'unsafe-inline'",
-    #   "https:",
-    #   "'nonce-dummynonce'",
-    #   ";",
-    #   "report-uri #{REPORT_URI}",
-    #   ";",
-    #   "report-to default"
-    # ]
-
-    headers["Content-Security-Policy-Report-Only"] = csp.join(" ")
+  # CSP for AMP Page
+  if /\.amp.html\z/.match(env["PATH_INFO"])
+    csp[:"default-src"].concat([
+      "https://*.ampproject.org",
+      "https://*.ampproject.net",
+      "https://adservice.google.com",
+    ])
+    csp[:"connect-src"].concat([
+      "https://*.ampproject.org",
+      "https://*.ampproject.net",
+      "https://*.google.com",
+      "https://*.google.co.jp",
+      "https://blog.jxck.io",
+    ])
+    csp[:"style-src"] = [
+      "'unsafe-inline'",
+    ]
   end
+
+  headers = {}
+  headers["Content-Security-Policy-Report-Only"] = csp.map{|k, v| "#{k} #{v.join(" ")};" }.join(" ")
   [399, headers, []]
 end
