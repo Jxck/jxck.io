aeaf9a2f5fd8ef2245b67732db6b27310dd2ecc7
Jxck <block.rxckin.beats@gmail.com>
2020-12-24 17:27:48 +0900

    [h2o] rewrite amppkg.rb with reproxy

--- .mruby.handler/amppkg.rb
+++ .mruby.handler/amppkg.rb
@@ -1,36 +1,18 @@
 Proc.new do |env|
-  logger = env["rack.errors"]
   path = env["PATH_INFO"]
-
-  # no amp page request
-  unless /\.amp.html\z/.match(path)
-    # do nothing, delegate as normal access
-    next [399, {}, []]
-  end
-
   amp_cache_transform = env["HTTP_AMP_CACHE_TRANSFORM"]
 
-  # no AMP-Cache-Transform request
-  if amp_cache_transform.nil?
-    # do nothing, delegate as normal access
-    next [399, {"vary" => "amp-cache-transform, accept"}, []]
+  # amp page request
+  if /\.amp.html\z/.match(path)
+    if amp_cache_transform.nil?
+      # delegate as normal access with vary header
+      next [399, {"vary" => "amp-cache-transform, accept"}, []]
+    else
+      # reproxy to backend amppackager
+      next [307, {"x-reproxy-url" => "http://127.0.0.1:10000/priv/doc/https://blog.jxck.io#{path}"}, []]
+    end
   end
 
-  logger.p(">>>>>>> #{path}")
-  req_headers = env.reduce({}){|acc, header|
-    k, v = header
-    if k.start_with?("HTTP_")
-      k = k[5..-1].downcase.gsub("_", "-")
-      acc[k] = v
-      logger.p("#{k}: #{v}")
-    end
-    acc
-  }
-  logger.p("<<<<<<<<<<<<<<<<<<<<<")
-
-  # forward to amppkg
-  req = http_request("http://127.0.0.1:10000/priv/doc/https://blog.jxck.io#{path}", {headers: req_headers})
-  status, headers, body = req.join
-  logger.p(status, '================')
-  [status, headers, body]
+  # do nothing, delegate as normal access
+  [399, {}, []]
 end
