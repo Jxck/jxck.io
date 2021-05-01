eb2c7a28677fbb58f26f9e1713beae5913c574b2
Jxck <block.rxckin.beats@gmail.com>
2021-05-01 18:09:17 +0900

    [labs] up workbox

--- labs.jxck.io/service-worker/workbox/sw.js
+++ labs.jxck.io/service-worker/workbox/sw.js
@@ -1,42 +1,38 @@
-importScripts('assets/workbox-v6.1.2/workbox-sw.js');
-importScripts('assets/workbox-v6.1.2/workbox-core.prod.js');
-importScripts('assets/workbox-v6.1.2/workbox-routing.prod.js');
-importScripts('assets/workbox-v6.1.2/workbox-strategies.prod.js');
-importScripts('assets/workbox-v6.1.2/workbox-precaching.prod.js');
+importScripts('assets/workbox-v6.1.2/workbox-sw.js')
+importScripts('assets/workbox-v6.1.2/workbox-core.prod.js')
+importScripts('assets/workbox-v6.1.2/workbox-routing.prod.js')
+importScripts('assets/workbox-v6.1.2/workbox-strategies.prod.js')
+importScripts('assets/workbox-v6.1.2/workbox-precaching.prod.js')
 
-console.log(workbox);
+console.log(workbox)
 
-
-const {PrecacheController} = workbox.precaching
+const {PrecacheController}    = workbox.precaching
 const {registerRoute, Router} = workbox.routing
-const {CacheFirst} = workbox.strategies
+const {CacheFirst}            = workbox.strategies
+
 
 console.log(Router)
 
-const router = new Router();
+const router = new Router()
 
 
-//import {registerRoute} from 'workbox-routing';
-//import {CacheFirst} from 'workbox-strategies';
+//import {registerRoute} from 'workbox-routing'
+//import {CacheFirst} from 'workbox-strategies'
 //
 //registerRoute(
 //  ({request}) => request.destination === 'style',
 //  new CacheFirst()
-//);
+//)
 
-const precacheController = new PrecacheController();
+const precacheController = new PrecacheController()
 precacheController.addToCacheList([
-  {url: "assets/a.css?v1", revision: null},
-  {url: "assets/b.css?v1", revision: null},
-  {url: "assets/c.css?v1", revision: null},
-  {url: "assets/x.js?v1",  revision: null},
-  {url: "assets/y.js?v1",  revision: null},
-  {url: "assets/z.js?v1",  revision: null},
+  {url: "/service-worker/workbox/assets/a.css?v1", revision: null},
+  {url: "/service-worker/workbox/assets/b.css?v1", revision: null},
+  {url: "/service-worker/workbox/assets/c.css?v1", revision: null},
+  {url: "/service-worker/workbox/assets/x.js?v1",  revision: null},
+  {url: "/service-worker/workbox/assets/y.js?v1",  revision: null},
+  {url: "/service-worker/workbox/assets/z.js?v1",  revision: null},
 ])
-
-
-
-
 
 router.registerRoute(
   ({url}) => {
@@ -45,12 +41,8 @@ router.registerRoute(
   new CacheFirst()
 )
 
-
-
-
 self.addEventListener('install', (event) => {
   console.log(event)
-
   precacheController.install(event)
 })
 
@@ -61,13 +53,11 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   console.log(event.request)
-
-  const responsePromise = router.handleRequest(event);
+  const responsePromise = router.handleRequest(event)
   console.log(responsePromise)
   if (responsePromise) {
     // Router found a route to handle the request
-    event.respondWith(responsePromise);
+    event.respondWith(responsePromise)
   }
   return
 })
-
