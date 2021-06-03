bce8e80885a449fbee77fbc7a23648308b85b5dc
Jxck <block.rxckin.beats@gmail.com>
2021-06-03 23:35:45 +0900

    [labs] fledge

--- labs.jxck.io/fledge/fledge.js
+++ labs.jxck.io/fledge/fledge.js
@@ -1,23 +1,25 @@
 const interestGroup = {
   name:                     "FLEDGE_TRIAL",
   owner:                    "https://publisher.labs.jxck.io",
+
+  // x-allow-fledge: true
   biddingLogicUrl:          "https://publisher.labs.jxck.io/fledge/bidding_logic.js",
+
+  // x-allow-fledge: true
   trustedBiddingSignalsUrl: "https://publisher.labs.jxck.io/fledge/bidding_signal.json",
-  trustBiddingSignalsKeys:  [
-    "bidding_signals_keys"
-  ],
-  userBiddingSignals: {
-    "user_bidding_signals": "user_bidding_signals"
-  },
+  trustBiddingSignalsKeys:  ["bidding_signals_keys"],
+
+  dailyUpdateUrl: "", // not implemented yets
+  userBiddingSignals:       {"user_bidding_signals": "user_bidding_signals"},
   ads: [
     {
-      renderUrl: "https://labs.jxck.io/fledge/shopping-ad.html",
+      renderUrl: "https://advertiser.labs.jxck.io/fledge/shopping-ad.html",
       metadata: {
         type: "shopping"
       }
     },
     {
-      renderUrl: "https://labs.jxck.io/fledge/travel-ad.html",
+      renderUrl: "https://advertiser.labs.jxck.io/fledge/travel-ad.html",
       metadata: {
         type: "travel"
       }
@@ -26,9 +28,11 @@ const interestGroup = {
 }
 
 const auctionConfig = {
-  seller:              "https://publisher.labs.jxck.io",
+  seller:              "https://publisher.labs.jxck.io", // should same as decisionLogicUrl's origin
+
+  // x-allow-fledge: true
   decisionLogicUrl:    "https://publisher.labs.jxck.io/fledge/decision_logic.js",
-  interestGroupBuyers: ["https://publisher.labs.jxck.io"],
+  interestGroupBuyers: ["https://publisher.labs.jxck.io"], // * is not supported yet
   auctionSignals:      {auction_signals: "auction_signals"},
   sellerSignals:       {seller_signals:  "seller_signals"},
   perBuyerSignals:     {"https://publisher.labs.jxck.io": {per_buyer_signals: "per_buyer_signals"}}
