f3eb84977f0c12e0ae6c5f32339e1043fe58fff0
Jxck <block.rxckin.beats@gmail.com>
2021-06-04 00:55:47 +0900

    [labs] separate domain for fledge

--- labs.jxck.io/fledge/fledge.js
+++ labs.jxck.io/fledge/fledge.js
@@ -1,15 +1,15 @@
 const interestGroup = {
   name:                     "FLEDGE_TRIAL",
-  owner:                    "https://publisher.labs.jxck.io",
+  owner:                    "https://adtech.labs.jxck.io",
 
   // x-allow-fledge: true
-  biddingLogicUrl:          "https://publisher.labs.jxck.io/fledge/bidding_logic.js",
+  biddingLogicUrl:          "https://adtech.labs.jxck.io/fledge/bidding_logic.js",
 
   // x-allow-fledge: true
-  trustedBiddingSignalsUrl: "https://publisher.labs.jxck.io/fledge/bidding_signal.json",
+  trustedBiddingSignalsUrl: "https://adtech.labs.jxck.io/fledge/bidding_signal.json",
   trustBiddingSignalsKeys:  ["bidding_signals_keys"],
 
-  dailyUpdateUrl: "", // not implemented yets
+  dailyUpdateUrl:           "", // not implemented yets
   userBiddingSignals:       {"user_bidding_signals": "user_bidding_signals"},
   ads: [
     {
@@ -28,14 +28,14 @@ const interestGroup = {
 }
 
 const auctionConfig = {
-  seller:              "https://publisher.labs.jxck.io", // should same as decisionLogicUrl's origin
+  seller:              "https://adtech.labs.jxck.io", // should same as decisionLogicUrl's origin
 
   // x-allow-fledge: true
-  decisionLogicUrl:    "https://publisher.labs.jxck.io/fledge/decision_logic.js",
-  interestGroupBuyers: ["https://publisher.labs.jxck.io"], // * is not supported yet
+  decisionLogicUrl:    "https://adtech.labs.jxck.io/fledge/decision_logic.js",
+  interestGroupBuyers: ["https://adtech.labs.jxck.io"], // * is not supported yet
   auctionSignals:      {auction_signals: "auction_signals"},
   sellerSignals:       {seller_signals:  "seller_signals"},
-  perBuyerSignals:     {"https://publisher.labs.jxck.io": {per_buyer_signals: "per_buyer_signals"}}
+  perBuyerSignals:     {"https://adtech.labs.jxck.io": {per_buyer_signals: "per_buyer_signals"}}
 }
 
 const kSecsPerDay = 10000
