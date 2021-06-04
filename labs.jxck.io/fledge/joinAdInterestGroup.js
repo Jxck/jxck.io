5c477e182054c6801e47230695a4649d7a69b508
Jxck <block.rxckin.beats@gmail.com>
2021-06-04 15:26:35 +0900

    [labs] separate join/run file

---
+++ labs.jxck.io/fledge/joinAdInterestGroup.js
@@ -0,0 +1,34 @@
+const interestGroup = {
+  name: "shopping",
+  owner: "https://adtech.labs.jxck.io", // dsp
+
+  // x-allow-fledge: true
+  biddingLogicUrl: "https://adtech.labs.jxck.io/fledge/bidding_logic.js",
+
+  // x-allow-fledge: true
+  trustedBiddingSignalsUrl: "https://adtech.labs.jxck.io/fledge/bidding_signal.json",
+  trustBiddingSignalsKeys: ["bidding_signals_keys"],
+
+  dailyUpdateUrl: "", // not implemented yets
+  userBiddingSignals: { "user_bidding_signals": "user_bidding_signals" },
+  ads: [
+    {
+      renderUrl: "https://advertiser.labs.jxck.io/fledge/shopping-ad.html",
+      metadata: {
+        type: "shopping"
+      }
+    },
+    {
+      renderUrl: "https://advertiser.labs.jxck.io/fledge/travel-ad.html",
+      metadata: {
+        type: "travel"
+      }
+    }
+  ]
+}
+
+document.addEventListener("DOMContentLoaded", async (e) => {
+  console.log(e)
+  const kSecsPerDay = 10000
+  console.log(await navigator.joinAdInterestGroup(interestGroup, kSecsPerDay))
+})
---
+++ labs.jxck.io/fledge/runAdAuction.js
@@ -0,0 +1,26 @@
+const auctionConfig = {
+  seller: "https://adtech.labs.jxck.io", // should https & same as decisionLogicUrl's origin
+
+  // x-allow-fledge: true
+  decisionLogicUrl: "https://adtech.labs.jxck.io/fledge/decision_logic.js",
+  interestGroupBuyers: [
+    // * is not supported yet
+    "https://adtech.labs.jxck.io",
+    "https://travel.labs.jxck.io",
+    "https://shopping.labs.jxck.io",
+  ],
+  auctionSignals: { auction_signals: "auction_signals" },
+  sellerSignals: { seller_signals: "seller_signals" },
+  perBuyerSignals: {
+    // interestGroupByers にリストされていること
+    "https://adtech.labs.jxck.io": { per_buyer_signals: "per_buyer_signals" },
+    "https://travel.labs.jxck.io": { per_buyer_signals: "per_buyer_signals" },
+    "https://shopping.labs.jxck.io": { per_buyer_signals: "per_buyer_signals" },
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async (e) => {
+  console.log(e)
+  const adAuctionResult = await navigator.runAdAuction(auctionConfig)
+  console.log({ adAuctionResult })
+})
