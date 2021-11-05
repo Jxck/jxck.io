10f2f46490323ff15c68fa465e18b69b6644ad30
Jxck <block.rxckin.beats@gmail.com>
2021-11-06 04:15:52 +0900

    [.src] fmt

--- .src/markdown/test/test.js
+++ .src/markdown/test/test.js
@@ -1,14 +1,14 @@
-import { encode, decode, node, dump } from "../index.js"
+import { encode, decode, traverse, node, dump } from "../index.js"
 import { deepStrictEqual } from "assert";
 import fs from "fs";
 
 const text = fs.readFileSync("./test.txt").toString()
-const cases = text.split('='.repeat(30))
+const cases = text.split(`=`.repeat(30))
   .filter((line) => {
     return !line.startsWith("\n//")
   })
   .map((block) => {
-    const [md, expected] = block.split('-'.repeat(30))
+    const [md, expected] = block.split(`-`.repeat(30))
     return [md, expected.trim()]
   })
 
@@ -17,30 +17,28 @@ for (const [md, expected] of cases) {
     // console.log(md)
     const ast = decode(md)
     // dump(ast)
-    const { html, toc } = encode(ast, { base: './' })
+    const { html, toc } = encode(ast, { base: `./` })
     // console.log({expected})
     // console.log({html})
-    deepStrictEqual(html, expected + '\n', html)
-    console.log('.')
+    deepStrictEqual(html, expected + `\n`, html)
+    console.log(`.`)
   } catch ({ message }) {
     deepStrictEqual(message, expected, expected)
   }
 }
 
-
-
 function api() {
-  const root = node({name: `root`, type: `block`})
-  
-  const div = node({name: `div`, type: `block`})
+  const root = node({ name: `root`, type: `block` })
+
+  const div = node({ name: `div`, type: `block` })
   div.addText(`test div`)
   root.appendChild(div)
 
-  const p = node({name: `p`, type: `block`})
+  const p = node({ name: `p`, type: `block` })
   p.addText(`test p`)
   root.appendChild(p)
 
-  const pre = node({name: `pre`, type: `block`})
+  const pre = node({ name: `pre`, type: `block` })
   pre.addText(`test pre`)
   p.insertBefore(pre)
 
@@ -72,4 +70,5 @@ function api() {
     `leave:p`
   ])
 }
+
 api()
