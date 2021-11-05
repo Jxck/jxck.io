3cec731990a78bd85cbc444dbc76c8182fcb96d4
Jxck <block.rxckin.beats@gmail.com>
2021-11-06 01:06:00 +0900

    [script] remove unused

--- .script/document/document.rb
+++
@@ -1,7 +0,0 @@
-require "./.script/document/article.rb"
-require "./.script/document/entry.rb"
-require "./.script/document/episode.rb"
-require "./.script/document/page.rb"
-
-module Document
-end
--- Gemfile
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-source "https://rubygems.org"
-
-#gem "md2indesign", github: "jxck/md2indesign"
-gem "md2indesign", path: "./.script/md2indesign"
