9b3f470ae66e8d7413688421d120672cc8778c21
Jxck <block.rxckin.beats@gmail.com>
2022-01-22 01:30:49 +0900

    [search] up

--- blog.jxck.io/search/search.sh
+++ blog.jxck.io/search/search.sh
@@ -1,2 +1,2 @@
 cd ../../
-HTTP_HOST="blog.jxck.io" QUERY_STRING="q=$1 $2" ruby ./blog.jxck.io/searches/search.cgi
+HTTP_HOST="blog.jxck.io" QUERY_STRING="q=$1 $2" ruby ./blog.jxck.io/search/search.cgi
