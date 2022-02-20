86a6202e4f33f4b61f636f0674ac746b0ed8ccf0
Jxck <block.rxckin.beats@gmail.com>
2022-02-20 23:39:53 +0900

    build

--- id3all.sh
+++ id3all.sh
@@ -1314,3 +1314,16 @@ eyeD3 --title "ep93 Web3" \
 
 
 
+eyeD3 --remove-all ../files.mozaic.fm/mozaic-ep94.mp3
+
+eyeD3 --title "ep94 Monthly Web 202202" \
+      --track 102 \
+      --artist 'Jxck' \
+      --album 'mozaic.fm' \
+      --genre 'Podcast' \
+      --add-image ./www.jxck.io/assets/img/mozaic.jpeg:FRONT_COVER \
+      --to-v2.3 \
+      ../files.mozaic.fm/mozaic-ep94.mp3
+
+
+
