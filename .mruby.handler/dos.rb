be53c02e4cb424028cd371f50478ef97480bad74
Jxck <block.rxckin.beats@gmail.com>
2022-03-20 05:36:25 +0900

    [h2o] up dos.rb

--- .mruby.handler/dos.rb
+++ .mruby.handler/dos.rb
@@ -1,8 +1,8 @@
 require "dos_detector.rb"
 DoSDetector.new({
   :strategy => DoSDetector::CountingStrategy.new({
-    :period     => 30,
-    :threshold  => 15,
-    :ban_period => 3000,
+    :period     => 10,
+    :threshold  => 100,
+    :ban_period => 300,
   }),
 })
