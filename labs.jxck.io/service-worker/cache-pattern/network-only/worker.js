04398f304c07d344d871897989d50ad589dee715
Jxck <block.rxckin.beats@gmail.com>
2022-03-22 13:30:48 +0900

    [labs] add network-only

---
+++ labs.jxck.io/service-worker/cache-pattern/network-only/worker.js
@@ -0,0 +1,17 @@
+console.info('worker')
+
+self.addEventListener('install', (e) => {
+  console.info(e.type, e)
+  e.waitUntil(skipWaiting())
+})
+
+self.addEventListener('activate', (e) => {
+  console.info(e.type, e)
+  e.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', (e) => {
+  console.log(e)
+  // e.respondWith(fetch(e.request));
+  return
+})
