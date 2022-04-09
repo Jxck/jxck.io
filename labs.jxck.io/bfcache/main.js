9feab33a27ce0d5e70f22b17cab9c7f83942efe5
Jxck <block.rxckin.beats@gmail.com>
2022-04-09 17:36:30 +0900

    [labs] up bfcache

--- labs.jxck.io/bfcache/main.js
+++ labs.jxck.io/bfcache/main.js
@@ -4,12 +4,17 @@ const $$ = document.querySelectorAll.bind(document)
 EventTarget.prototype.on = EventTarget.prototype.addEventListener
 
 window.on('load', async (e) => {
+  console.log(e.type, e)
   const res = await fetch('index.html')
   console.log(res)
 })
 
+document.on('DOMContentLoaded', (e) => {
+  console.log(e.type, e)
+})
+
 window.on('pageshow', (e) => {
-  console.log(e)
+  console.log(e.type, e)
   if (e.persisted) {
     console.log('BFCache Hit')
   }
