60a172ebfa4342d79728777041fdbd1d5a8429dc
Jxck <block.rxckin.beats@gmail.com>
2022-08-08 00:06:05 +0900

    build

--- id3all.sh
+++ id3all.sh
@@ -1405,3 +1405,16 @@ eyeD3 --title "ep100 State of mozaic.fm" \
 
 
 
+eyeD3 --remove-all ../files.mozaic.fm/mozaic-ep101.mp3
+
+eyeD3 --title "ep101 Passwordless" \
+      --track 109 \
+      --artist 'Jxck' \
+      --album 'mozaic.fm' \
+      --genre 'Podcast' \
+      --add-image ./www.jxck.io/assets/img/mozaic.jpeg:FRONT_COVER \
+      --to-v2.3 \
+      ../files.mozaic.fm/mozaic-ep101.mp3
+
+
+
