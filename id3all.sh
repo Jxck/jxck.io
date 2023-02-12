5bfaf3c1653587bb7b3f15a1376c8ae02bc976eb
Jxck <block.rxckin.beats@gmail.com>
2023-02-12 12:35:29 +0900

    build

--- id3all.sh
+++ id3all.sh
@@ -1574,3 +1574,16 @@ eyeD3 --title "ep113 Monthly Platform 202301" \
 
 
 
+eyeD3 --remove-all ../files.mozaic.fm/mozaic-ep114.mp3
+
+eyeD3 --title "ep114 Monthly Ecosystem 202302" \
+      --track 122 \
+      --artist 'Jxck' \
+      --album 'mozaic.fm' \
+      --genre 'Podcast' \
+      --add-image ./www.jxck.io/assets/img/mozaic.jpeg:FRONT_COVER \
+      --to-v2.3 \
+      ../files.mozaic.fm/mozaic-ep114.mp3
+
+
+
