2220b3e2be2413476ce6e2d35bdff874fa38e6aa
Jxck <block.rxckin.beats@gmail.com>
2023-10-02 14:44:46 +0900

    build

--- id3all.sh
+++ id3all.sh
@@ -1808,3 +1808,16 @@ eyeD3 --title "ep131 Drive Talk: San Francisco" \
 
 
 
+eyeD3 --remove-all ../files.mozaic.fm/mozaic-ep132.mp3
+
+eyeD3 --title "ep129 Monthly Platform 202308" \
+      --track 140 \
+      --artist 'Jxck' \
+      --album 'mozaic.fm' \
+      --genre 'Podcast' \
+      --add-image ./www.jxck.io/assets/img/mozaic.jpeg:FRONT_COVER \
+      --to-v2.3 \
+      ../files.mozaic.fm/mozaic-ep132.mp3
+
+
+
