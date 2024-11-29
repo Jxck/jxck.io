be9cfa35b26e7aabe4ee5392a21914e1e38d45bf
Jxck <jxck@jxck.io>
2024-11-29 14:38:58 +0900

    build

--- id3all.sh
+++ id3all.sh
@@ -2250,3 +2250,16 @@ eyeD3 --title "ep165 Monthly Ecosystem 202411" \
 
 
 
+eyeD3 --remove-all ../files.mozaic.fm/mozaic-ep166.mp3
+
+eyeD3 --title "ep166 Monthly Platform 202411" \
+      --track 174 \
+      --artist 'Jxck' \
+      --album 'mozaic.fm' \
+      --genre 'Podcast' \
+      --add-image ./www.jxck.io/assets/img/mozaic.jpeg:FRONT_COVER \
+      --to-v2.3 \
+      ../files.mozaic.fm/mozaic-ep166.mp3
+
+
+
