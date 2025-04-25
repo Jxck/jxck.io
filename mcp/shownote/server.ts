1bd7e9fb0707be678dede3a15b6473d5897bb8a3
Jxck <jxck@jxck.io>
2025-04-25 20:56:20 +0900

    [mcu] up shownote to 100

--- mcp/shownote/server.ts
+++ mcp/shownote/server.ts
@@ -19,7 +19,7 @@ server.addTool({
 server.addTool({
   name: "get_vtt_chunk",
   description:
-    "指定した VTT を 50 行に区切り、指定したインデックス部分の書き起こしを取得します",
+    "指定した VTT を 100 行に区切り、指定したインデックス部分の書き起こしを取得します",
   parameters: z.object({
     episode_number: z.number().describe("エピソード番号"),
     index: z.number().describe("取得する VTT のインデックス (0 から始まる)"),
@@ -30,7 +30,7 @@ server.addTool({
 server.addTool({
   name: "replace_vtt_chunk",
   description:
-    "指定した VTT を 50 行に区切り、指定したインデックスの部分の書き起こしを、指定した内容で上書きします",
+    "指定した VTT を 100 行に区切り、指定したインデックスの部分の書き起こしを、指定した内容で上書きします",
   parameters: z.object({
     episode_number: z.number().describe("エピソード番号"),
     index: z.number().describe("上書きする VTT のインデックス (0 から始まる)"),
