b0ac180719e29d0367bc60072e95e27f87e785db
Jxck <jxck@jxck.io>
2025-12-01 15:48:45 +0900

    build

--- id3all.sh
+++ id3all.sh
@@ -2575,3 +2575,16 @@ eyeD3 --title "ep190 Monthly Ecosystem 202511" \
 
 
 
+eyeD3 --remove-all ../files.mozaic.fm/mozaic-ep191.mp3
+
+eyeD3 --title "ep191 Privacy Sandbox" \
+      --track 199 \
+      --artist 'Jxck' \
+      --album 'mozaic.fm' \
+      --genre 'Podcast' \
+      --add-image ./www.jxck.io/assets/img/mozaic.jpeg:FRONT_COVER \
+      --to-v2.3 \
+      ../files.mozaic.fm/mozaic-ep191.mp3
+
+
+
