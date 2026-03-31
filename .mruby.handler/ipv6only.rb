5da167afe8ea017c576c0ac24f89ee08e7d7001e
Jxck <jxck@jxck.io>
2026-03-31 21:49:11 +0900

    [h2o] up

--- .mruby.handler/ipv6only.rb
+++ .mruby.handler/ipv6only.rb
@@ -1,27 +1,27 @@
 Proc.new do |env|
   addr = env["REMOTE_ADDR"]
-  unless addr.include?(":")
-    body = <<~TEXT
-      本サイトは、 IPv6 での接続のみを受け付けるように制限しています。
-      あなたのアクセスは IPv4 であるため、別のネットワークからアクセスして下さい。
+  # IPv6 ならそのまま通す
+  next [399, {}, []] if addr.include?(":")
+  # IPv4 ならエラーにする
+  body = <<~TEXT
+    本サイトは、 IPv6 での接続のみを受け付けるように制限しています。
+    あなたのアクセスは IPv4 であるため、別のネットワークからアクセスして下さい。
 
-      あなたのアドレス: #{addr}
+    あなたのアドレス: #{addr}
 
-      この制限は、あなたの身の回りに IPv4 のみの環境がどのくらい残っているかを啓蒙する目的で行っています。
-      詳細な背景については以下を参照してください。
+    この制限は、あなたの身の回りに IPv4 のみの環境がどのくらい残っているかを啓蒙する目的で行っています。
+    #{
+    # 詳細な背景については以下を参照してください。
+    # https://blog.jxck.io/entries/2026-04-01/ipv6-only.html
+    # ただし、この記事も IPv6 で接続しないと見られません。
+  }スマホのキャリア回線でテザリングするか、 Cloudflare WARP を入れるのが手軽です。
 
-      https://blog.jxck.io/entries/2026-04-01/ipv6-only.html
-
-      ただし、この記事も IPv6 で接続しないと見られません。
-      スマホのキャリア回線でテザリングするか、 Cloudflare WARP を入れるのが手軽です。
-
-      Jxck
-    TEXT
-    next [
-      403,
-      {"Content-Type" => "text/plain; charset=utf-8"},
-      [body]
-    ]
-  end
-  [399, {}, []]
+    Jxck
+  TEXT
+  # 403 でエラーメッセージを返す
+  [
+    403,
+    {"Content-Type" => "text/plain; charset=utf-8"},
+    [body]
+  ]
 end
