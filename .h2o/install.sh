74fdecb186e9b5815ad017ccc9c762d47af64ad0
Jxck <jxck@jxck.io>
2026-08-25 18:32:23 +0900

    [h2] link openssl3 to h2o

--- .h2o/install.sh
+++ .h2o/install.sh
@@ -17,8 +17,17 @@ cd $BASE/pkg
 git clone --depth=1 https://github.com/h2o/h2o
 
 cd $BASE/pkg/h2o
+# OpenSSL は system (3.0.13、ML-KEM 無し) ではなく linuxbrew (3.5+) にリンクする。
+# TCP 側 TLS の X25519MLKEM768 (ポスト量子鍵交換) は libcrypto 3.5+ が必要
+# (mozaic.fm plan 20260825-1543。3.0.13 リンクでは server が MLKEM を選んだ瞬間に
+# alert 80 で落ちる障害を実測)。INSTALL_RPATH が無いと install 後の binary が
+# linuxbrew の libssl を見つけられず起動しない。
+OPENSSL_DIR=/home/linuxbrew/.linuxbrew/opt/openssl@3
 cmake \
   -DCMAKE_INSTALL_PREFIX=$BASE/local \
+  -DOPENSSL_ROOT_DIR=$OPENSSL_DIR \
+  -DCMAKE_INSTALL_RPATH=$OPENSSL_DIR/lib \
+  -DCMAKE_BUILD_WITH_INSTALL_RPATH=on \
   -DWITH_MRUBY=on .
 make -j$(nproc)
 make install
